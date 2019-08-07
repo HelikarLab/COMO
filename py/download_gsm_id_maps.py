c41c1e0e24cd48de8c2ce3249215f44e3758c171
ZZHAO1985 <zzhao1985@gmail.com>
2019-08-07 17:05:54 -0500

    test for step 1

--- py/download_gsm_id_maps.py
+++ py/download_gsm_id_maps.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import importlib
-importlib.import_module('step1_read_raw_data')
-#from step1_read_raw_data import *
+#importlib.import_module('step1_read_raw_data')
+from step1_read_raw_data import *
 
 platforms = ['GPL96','GPL97','GPL8300']
 maps_dict = download_gsm_id_maps(datadir,gse,platforms)
