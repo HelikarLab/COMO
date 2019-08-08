31ae80966efcbc97d9eb4bc65202218c38876ee0
ZZHAO1985 <zzhao1985@gmail.com>
2019-08-08 13:36:56 -0500

    z score

--- py/GSEpipeline.py
+++ py/GSEpipeline.py
@@ -3,6 +3,7 @@
 import re
 import os
 import pandas as pd
+import numpy as np
 import GEOparse
 from rpy2.robjects.packages import importr
 from rpy2.robjects import r, pandas2ri
@@ -314,3 +315,14 @@ class GSEproject:
         print('Full table saved to:\n{}'.format(filefullpath))
         return df_clean_sc500
 
+    def calculate_z_score(self,df,saveas=None):
+        cols = list(df)
+        result = pd.DataFrame([],index=df.index)
+        for col in cols:
+            if not '.gz.' in col:
+                score = np.log2(df[col])
+                result[col] = (score-score.mean())/score.std(ddof=0)
+        if not saveas is None:
+            filefullpath = os.path.join(self.genedir,'{}_data_z.csv'.format(self.gsename))
+            result.to_csv(filefullpath)
+        return result
