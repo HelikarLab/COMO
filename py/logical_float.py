c39eb107cf031ef1ab8448b27d4d480c1eafdab4
ZZHAO1985 <zzhao1985@gmail.com>
2019-10-03 11:14:10 -0500

    update from jupyter

--- py/logical_float.py
+++ py/logical_float.py
@@ -1,3 +1,27 @@
+import sys;
+# sys.path.extend(['E:\\reconstruction', 'E:\\cobamp', 'E:\\reconstruction\\src', 'E:\\cobamp\\src', 'E:/reconstruction'])
+import cobra
+import framed
+import cobamp
+import pandas as pd
+import numpy as np
+import pickle
+import scipy as sci
+import os
+import re
+
+# for testing the algorithms
+from cobamp.wrappers import MatFormatReader
+from cobamp.wrappers import COBRAModelObjectReader
+from troppo.methods.imat import IMAT
+from troppo.methods.gimme import GIMME
+from troppo.methods.tINIT import tINIT
+from troppo.methods.fastcore import FASTcore
+from troppo.reconstruction_properties import FastcoreProperties, tINITProperties, GIMMEProperties, IMATProperties
+from troppo.utilities.statistics import normalize, z_score
+
+from project import configs
+
 testexp = '(( 2977 ) and ( 2983 )) or 4882 or (( 2982 ) and ( 2974 )) or 2986 or 2984 or 4881 or (( 2977 ) and ( 2974 )) or 3000 or (( 2982 ) and ( 2983 ))'
 
 def float_logical_exp(expressionIn, level=0):
@@ -33,10 +57,90 @@ def float_logical_exp(expressionIn, level=0):
 
     return expressionOut
 
-if not (testexp[0] == '(' and testexp[-1] == ')'):
-    testexp = '('+testexp+')'
-outexp=float_logical_exp(testexp)
 
-outexp = outexp.replace('{','(')
-outexp = outexp.replace('}',')')
-print(eval(outexp))
+generalmodelmat = os.path.join(configs.datadir,'GeneralModel.mat')
+
+model_cobra = cobra.io.load_matlab_model(generalmodelmat)
+
+mat = sci.io.loadmat(generalmodelmat)['Teff']
+model = MatFormatReader(mat)
+S = model.S
+lb, ub = model.get_model_bounds(False, True)
+rx_names = model.get_reaction_and_metabolite_ids()[0]
+
+idx_objective = rx_names.index('biomass_reaction_Mphage')
+properties = GIMMEProperties(
+    exp_vector=exp_vector,#np.array(gimme_data['0']),
+    obj_frac=0.9,
+    objectives= [{idx_objective:1}],
+    preprocess=True,
+    flux_threshold=0.9
+)
+algorithm = GIMME(S, lb.astype(float), ub.astype(float), properties)
+model_GIMME = algorithm.run()
+
+model_GIMME_final = model_cobra.copy() # this is done since it alters the original model_cobra; this way is to guarantee that a new model is changed instead of the original model
+r_ids = [r.id for r in model_GIMME_final.reactions]
+to_remove_ids = [r_ids[r] for r in np.where(model_GIMME==0)[0]]
+model_GIMME_final.remove_reactions(to_remove_ids,True) # this is to get the ids of the reactions to be removed in the model; True is to remove the pending genes/metabolites that with the removal of the reaction can no longer be connected in the network
+
+print('1\'s: ' + str(len(np.where(model_GIMME==1)[0])))
+print('2\'s: ' + str(len(np.where(model_GIMME==2)[0])))
+print('Genes: ' + str(len(model_GIMME_final.genes)))
+print('Metabolites: ' + str(len(model_GIMME_final.metabolites)))
+print('Reactions: ' + str(len(model_GIMME_final.reactions)))
+print(model_GIMME_final.objective._get_expression())
+print(model_GIMME_final.optimize())
+
+
+csvfilepath = os.path.join(configs.datadir,'merged_Th1.csv')
+expressionData = pd.read_csv(csvfilepath)
+expressionData.rename(columns={'ENTREZ_GENE_ID':'Gene','Express':'Data'},inplace=True)
+expressionData = expressionData.loc[:,['Gene','Data']]
+singleGeneNames = expressionData[~expressionData.Gene.str.contains('//')].reset_index(drop=True)
+multipleGeneNames = expressionData[expressionData.Gene.str.contains('//')].reset_index(drop=True)
+breaksGeneNames = pd.DataFrame(columns=['Gene','Data'])
+print(singleGeneNames.shape)
+print(multipleGeneNames.shape)
+for index,row in multipleGeneNames.iterrows():
+    for genename in row['Gene'].split('///'):
+        breaksGeneNames = breaksGeneNames.append({'Gene':genename,'Data':row['Data']},ignore_index=True)
+print(breaksGeneNames.shape)
+GeneExpressions = singleGeneNames.append(breaksGeneNames,ignore_index=True)
+print(GeneExpressions.shape)
+GeneExpressions.set_index('Gene',inplace=True)
+# GeneExpressions
+
+expressionRxns = {}
+cnt = 0
+for rxn in model_cobra.reactions:
+    gene_reaction_rule = rxn.gene_reaction_rule
+    gene_ids = re.findall(r'\d+', gene_reaction_rule)
+    expressionRxns[rxn.id] = -1.0
+    if gene_reaction_rule == '':
+        continue
+    for id in gene_ids:
+        boolval = '-1'
+        if id in GeneExpressions.index:
+            boolval = '{}'.format(GeneExpressions.at[id, 'Data'])
+        gene_reaction_rule = gene_reaction_rule.replace('{}'.format(id), boolval, 1)
+
+    try:
+        gene_reaction_by_rule = float_logical_exp(gene_reaction_rule)
+        gene_reaction_by_rule = gene_reaction_by_rule.replace('{', '(')
+        gene_reaction_by_rule = gene_reaction_by_rule.replace('}', ')')
+        expressionRxns[rxn.id] = eval(gene_reaction_by_rule)
+    except:
+        print(gene_reaction_by_rule)
+        cnt += 1
+print(cnt)
+
+
+
+# if not (testexp[0] == '(' and testexp[-1] == ')'):
+#     testexp = '('+testexp+')'
+# outexp=float_logical_exp(testexp)
+#
+# outexp = outexp.replace('{','(')
+# outexp = outexp.replace('}',')')
+# print(eval(outexp))
