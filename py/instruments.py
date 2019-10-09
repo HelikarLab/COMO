f0b121b6c0e914599e15ba2c074b7c42778391bb
ZZHAO1985 <zzhao1985@gmail.com>
2019-10-09 14:44:06 -0500

    agilent disease data load, untested

--- py/instruments.py
+++ py/instruments.py
@@ -66,6 +66,24 @@ readaiglent <- function(addr,targets){
     ydf <- data.frame(yo)
     return(ydf)
 }
+fitagilent <- function(addr,target){
+    crd <- getwd()
+    setwd(addr)    
+    targets <- readTargets(target)
+    x <- read.maimages(targets, path="somedirectory", source="agilent",green.only=TRUE)
+    y <- backgroundCorrect(x, method="normexp", offset=16)
+    y <- normalizeBetweenArrays(y, method="quantile")
+    y.ave <- avereps(y, ID=y$genes$ProbeName)
+    f <- factor(targets$Condition, levels = unique(targets$Condition))
+    design <- model.matrix(~0 + f)
+    colnames(design) <- levels(f)
+    fit <- lmFit(y.ave, design)
+    contrast.matrix <- makeContrasts("Treatment1-Treatment2", "Treatment1-Treatment3", "Treatment2-Treatment1", levels=design)
+    fit2 = contrasts.fit(fit,contrast.matrix)
+    fit2 = eBayes(fit2)
+    data = topTable(fit2,number = "inf")
+    return(data)
+    }
 """
 
 agilentio = SignatureTranslatedAnonymousPackage(agilentstring, "agilentio")
