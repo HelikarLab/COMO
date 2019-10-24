37a776ae3dce7f59f8f6c49dd2677a02d77a9395
Zhongyuan Zhao <zzhao1985@gmail.com>
2019-10-24 12:22:30 -0500

    release version

---
+++ docker/pipelines/py/knock_out_simulation.py
@@ -0,0 +1,313 @@
+#!/usr/bin/python3
+import os
+import re
+import sys
+import getopt
+import time
+import pandas as pd
+import numpy as np
+import cobra
+import copy
+from cobra.flux_analysis import (single_gene_deletion, single_reaction_deletion,
+                                 double_gene_deletion, double_reaction_deletion, moma)
+from project import configs
+# from transcriptomic_gen import *
+# from proteomics_gen import *
+from instruments import fetch_entrez_gene_id
+
+def knock_out_simulation(datadir, model_file, inhibitors, drugDB):
+
+    if model_file[-4:] == '.xml':
+        model = cobra.io.read_sbml_model(os.path.join(datadir, model_file))
+    elif model_file[-4:] == '.mat':
+        model = cobra.io.load_matlab_model(os.path.join(datadir, model_file))
+    else:
+        print("Unsupported File Format of Model: {}".format(model_file))
+        return None
+
+    inhibitorsFullpath = os.path.join(datadir, inhibitors)
+    if os.path.isfile(inhibitorsFullpath):
+        DT_genes = pd.read_csv(os.path.join(datadir, inhibitors), header=None)
+        DT_genes.rename(columns={0:'Gene ID'},inplace=True)
+        DT_genes['Gene ID'] = DT_genes['Gene ID'].astype(str)
+    else:
+        # if inhibitors file does not exist, create a new one
+        DT_genes = pd.DataFrame(columns=['Gene ID'])
+        DT_genes['Gene ID'] = drugDB['ENTREZ_GENE_ID'].astype(str)
+        DT_genes.replace('-', np.nan, inplace=True)
+        DT_genes.dropna(axis=0,inplace=True)
+        DT_genes.to_csv(inhibitorsFullpath, header=False)
+
+    geneInd2genes = [x.id for x in model.genes]
+    print(len(geneInd2genes))
+    geneInd2genes = set(geneInd2genes)
+    print(len(geneInd2genes))
+    DT_model = set(DT_genes['Gene ID'].tolist()).intersection(geneInd2genes)
+    print(len(DT_model))
+    DT_model = list(DT_model)
+
+    model_opt = moma(model).to_frame()
+    # model_opt = model.optimize().to_frame()
+    model_opt[abs(model_opt) < 1e-8] = 0.0
+
+    HasEffects_Gene = []
+    for id in DT_model:
+        gene = model.genes.get_by_id(id)
+        for rxn in gene.reactions:
+            gene_reaction_rule = rxn.gene_reaction_rule
+            gene_ids = re.findall(r'\d+',gene_reaction_rule)
+            for gene_id in gene_ids:
+                if gene_id == id:
+                    boolval = 'False'
+                else:
+                    boolval = '{}'.format(model.genes.get_by_id(gene_id)._functional)
+                gene_reaction_rule = gene_reaction_rule.replace('{}'.format(gene_id), boolval, 1)
+            if not eval(gene_reaction_rule):
+                HasEffects_Gene.append(id)
+                break
+
+    fluxsolution = pd.DataFrame()
+    for id in HasEffects_Gene:
+        model_cp = copy.deepcopy(model)
+        gene = model_cp.genes.get_by_id(id)
+        gene.knock_out()
+        opt_model = moma(model_cp).to_frame()
+        # opt_model = model_cp.optimize().to_frame() #FBA
+        fluxsolution[id]=opt_model['fluxes']
+        del model_cp
+
+    # fluxsolution
+    fluxsolution[abs(fluxsolution) < 1e-8] = 0.0
+
+    fluxSolutionRatios = fluxsolution.div(model_opt['fluxes'], axis=0)
+    # fluxSolutionRatios
+    fluxSolutionDiffs = fluxsolution.sub(model_opt['fluxes'], axis=0)
+    # fluxSolutionDiffs
+
+    # HasEffects_Gene
+    return model, geneInd2genes, HasEffects_Gene, fluxsolution, fluxSolutionRatios, fluxSolutionDiffs
+
+
+def create_gene_pairs(datadir, model, geneInd2genes, fluxsolution, fluxSolutionRatios, fluxSolutionDiffs, HasEffects_Gene, RA_Down):
+    RA_down = pd.read_csv(os.path.join(datadir,RA_Down))
+    DAG_dis_genes = pd.DataFrame()
+    DAG_dis_genes['Gene ID'] = RA_down.iloc[:,0].astype(str)
+    # DAG_dis_genes
+
+    DAG_dis_met_genes = set(DAG_dis_genes['Gene ID'].tolist()).intersection(geneInd2genes)
+    # DAG_dis_met_genes
+
+    DAG_dis_met_rxnInd = []
+    Gene_i = []
+    for id in DAG_dis_met_genes:
+        gene = model.genes.get_by_id(id)
+        for rxn in gene.reactions:
+            DAG_dis_met_rxnInd.append(rxn.id)
+            Gene_i.append(id)
+
+    # DAG_dis_met_rxnInd
+    Gene_df = pd.DataFrame(Gene_i,columns=['Gene IDs'],index=DAG_dis_met_rxnInd)
+    # Gene_df
+
+    DAG_rxn_fluxRatio = fluxSolutionRatios.loc[DAG_dis_met_rxnInd]
+    DAG_rxn_fluxDiffs = fluxSolutionDiffs.loc[DAG_dis_met_rxnInd]
+    DAG_rxn_fluxValue = fluxsolution.loc[DAG_dis_met_rxnInd]
+    # DAG_rxn_fluxRatio
+
+    gene_mat_out = []
+    # Gene_i = DAG_dis_met_genes
+    # Rind_i = DAG_dis_met_rxnInd
+    for id in HasEffects_Gene:
+        pegene = pd.DataFrame()
+        pegene['Gene IDs'] = Gene_df['Gene IDs'].copy()
+        pegene['rxn_fluxRatio'] = DAG_rxn_fluxRatio[id].copy()
+        rxn_fluxDiffs = DAG_rxn_fluxDiffs[id].copy()
+        rxn_fluxValue = DAG_rxn_fluxValue[id].copy()
+        pegene['Gene'] = id
+        pegene = pegene.loc[(~pegene['rxn_fluxRatio'].isna()) & (abs(rxn_fluxDiffs) + abs(rxn_fluxValue) > 1e-6)]
+        # pegene.dropna(axis=0,subset=['rxn_fluxRatio'],inplace=True)
+        pegene.index.name='reaction'
+        pegene.reset_index(drop=False, inplace=True)
+        gene_mat_out.append(pegene)
+
+    Gene_Pairs = pd.concat(gene_mat_out, ignore_index=True)
+    return Gene_Pairs
+
+
+def score_gene_pairs(Gene_Pairs, filename):
+    p_model_genes = Gene_Pairs.Gene.unique()
+    d_score = pd.DataFrame([], columns=['score'])
+    for p_gene in p_model_genes:
+        data_p = Gene_Pairs.loc[Gene_Pairs['Gene'] == p_gene].copy()
+        # print(data_p)
+        total_aff = data_p['Gene IDs'].unique().size
+        # print(total_aff)
+        n_aff_down = data_p.loc[abs(data_p['rxn_fluxRatio']) < 0.99, 'Gene IDs'].unique().size
+        # print(n_aff_down)
+        n_aff_up = data_p.loc[abs(data_p['rxn_fluxRatio']) > 1.0, 'Gene IDs'].unique().size
+        # print(n_aff_up)
+        d_s = ((n_aff_down - n_aff_up) / total_aff)
+        # print(d_s)
+        d_score.at[p_gene, 'score'] = d_s
+
+    d_score.index.name = 'Gene'
+    d_score.to_csv(os.path.join(configs.datadir, filename))
+    return d_score
+
+
+def score_gene_pairs_diff(Gene_Pairs, fileFullPath):
+    p_model_genes = Gene_Pairs.Gene.unique()
+    d_score = pd.DataFrame([], columns=['score'])
+    for p_gene in p_model_genes:
+        data_p = Gene_Pairs.loc[Gene_Pairs['Gene'] == p_gene].copy()
+        # print(data_p)
+        total_aff = data_p['Gene IDs'].unique().size
+        # print(total_aff)
+        n_aff_down = data_p.loc[data_p['rxn_fluxRatio'] < -1e-8, 'Gene IDs'].unique().size
+        # print(n_aff_down)
+        n_aff_up = data_p.loc[data_p['rxn_fluxRatio'] > 1e-8, 'Gene IDs'].unique().size
+        # print(n_aff_up)
+        d_s = ((n_aff_down - n_aff_up) / total_aff)
+        # print(d_s)
+        d_score.at[p_gene, 'score'] = d_s
+
+    d_score.index.name = 'Gene'
+    d_score.to_csv(fileFullPath)
+    return d_score
+
+
+def load_Inhi_Fratio(filepath):
+    temp2  =pd.read_csv(filepath)
+    temp2.rename(columns={'gene_mat_out1':'Gene','gene_mat_out2':'Gene IDs','gene_mat_out3':'rxn_fluxRatio'},inplace=True)
+    temp2.Gene = temp2.Gene.astype(str)
+    temp2['Gene IDs'] = temp2['Gene IDs'].astype(str)
+    return temp2
+
+
+def repurposing_hub_preproc(drugFile):
+    drugDB = pd.read_csv(drugFile, sep='\t')
+    drugDB_new = pd.DataFrame()
+    for index, row in drugDB.iterrows():
+        if pd.isnull(row['Target']):
+            continue
+        for target in row['Target'].split(','):
+            drugDB_new = drugDB_new.append(
+                {'Name': row['Name'], 'MOA': row['MOA'], 'Target': target.strip(), 'Phase': row['Phase']},
+                ignore_index=True)
+    entrez_IDs = fetch_entrez_gene_id(drugDB_new['Target'].tolist(), input_db='Gene Symbol')
+    entrez_IDs.reset_index(drop=False, inplace=True)
+    drugDB_new['ENTREZ_GENE_ID'] = entrez_IDs['Gene ID']
+    drugDB_new = drugDB_new[['Name', 'MOA', 'Target', 'ENTREZ_GENE_ID', 'Phase']]
+    return drugDB_new
+
+
+def drug_repurposing(drugDB, d_score):
+    d_score['Gene'] = d_score['Gene'].astype(str)
+    d_score_geneSym = fetch_entrez_gene_id(d_score['Gene'].tolist(), input_db='Gene ID', output_db=['Gene Symbol'])
+    d_score.set_index('Gene', inplace=True)
+    d_score['Gene Symbol'] = d_score_geneSym['Gene Symbol']
+    d_score.reset_index(drop=False, inplace=True)
+    d_score_new = pd.DataFrame()
+    for index, row in d_score.iterrows():
+        target = row['Gene Symbol']
+        # print(target)
+        drugs = drugDB.loc[drugDB['Target'] == target, :]
+        # print(drugs)
+        drugs['d score'] = row['score']
+        d_score_new = d_score_new.append(drugs, ignore_index=True)
+
+    d_score_new.drop_duplicates(inplace=True)
+
+    return d_score_new
+
+
+def main(argv):
+    tissue_Spec_Model_file = 'Th1_Cell_SpecificModel4manuscript.mat'
+    inhibitors_file = 'Th1_inhibitors_Entrez.txt'
+    RA_Dn_file = 'RA_DOWN.txt'
+    RA_Up_file = 'RA_UP.txt'
+    drug_raw_file = 'Repurposing_Hub_export.txt'
+    folder = 'Th1'
+    try:
+        opts, args = getopt.getopt(argv, "ht:i:u:d:f:r:", ["tfile=", "ifile=", "upfile=", "downfile=", "folder=", "drugfile="])
+    except getopt.GetoptError:
+        print('python3 knock_out_simulation.py -t <tissue_model_file> -i <inhibitor_file> -u <up_reg_file> -d <down_reg_file> -f <output_folder> -r <repurpose drug raw file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('python3 knock_out_simulation.py -t <tissue_model_file> -i <inhibitor_file> -u <up_reg_file> -d <down_reg_file> -f <output_folder> -r <repurpose drug raw file>')
+            sys.exit()
+        elif opt in ("-t", "--tfile"):
+            tissue_Spec_Model_file = arg
+        elif opt in ("-i", "--ifile"):
+            inhibitors_file = arg
+        elif opt in ("-u", "--upfile"):
+            RA_Up_file = arg
+        elif opt in ("-d", "--downfile"):
+            RA_Dn_file = arg
+        elif opt in ("-f", "--folder"):
+            folder = arg
+        elif opt in ("-r", "--drugfile"):
+            drug_raw_file = arg
+    datadir = os.path.join(configs.datadir, folder)
+    print('Output folder: "{}"'.format(datadir))
+    print('Tissue Specific Model file is "{}"'.format(tissue_Spec_Model_file))
+    print('Tissue Specific Inhibitors file is "{}"'.format(inhibitors_file))
+
+
+    # preprocess repurposing hub data
+    drug_csv_file = 'Repurposing_Hub_Preproc.csv'
+    drugRawFile = os.path.join(configs.datadir, drug_raw_file)
+    drugFile = os.path.join(configs.datadir, drug_csv_file)
+    if not os.path.isfile(drugFile):
+        drugDB = repurposing_hub_preproc(drugRawFile)
+        drugDB.to_csv(drugFile, index=False)
+    else:
+        drugDB = pd.read_csv(drugFile)
+
+
+    # Knock Out Simulation
+    model, geneInd2genes, HasEffects_Gene, fluxsolution, fluxSolutionRatios, fluxSolutionDiffs = knock_out_simulation(datadir=configs.datadir,
+                                      model_file=tissue_Spec_Model_file,
+                                      inhibitors=inhibitors_file,
+                                      drugDB=drugDB)
+    Gene_Pairs_down = create_gene_pairs(configs.datadir,
+                                   model,
+                                   geneInd2genes,
+                                   fluxsolution, fluxSolutionRatios, fluxSolutionDiffs,
+                                   HasEffects_Gene,
+                                   RA_Down=RA_Dn_file)
+    Gene_Pairs_down.to_csv(os.path.join(datadir,'Gene_Pairs_Inhi_Fratio_DOWN.txt'),index=False)
+    Gene_Pairs_up = create_gene_pairs(configs.datadir,
+                                   model,
+                                   geneInd2genes,
+                                   fluxsolution, fluxSolutionRatios, fluxSolutionDiffs,
+                                   HasEffects_Gene,
+                                   RA_Down=RA_Up_file)
+    Gene_Pairs_up.to_csv(os.path.join(datadir,'Gene_Pairs_Inhi_Fratio_UP.txt'),index=False)
+    # print(geneInd2genes)
+    # print(fluxSolutionRatios)
+    # print(HasEffects_Gene)
+    # Gene_Pairs_down = load_Inhi_Fratio(os.path.join(datadir,'Gene_Pairs_Inhi_Fratio_DOWN.txt'))
+    # Gene_Pairs_up = load_Inhi_Fratio(os.path.join(datadir,'Gene_Pairs_Inhi_Fratio_UP.txt'))
+    d_score_down = score_gene_pairs(Gene_Pairs_down, os.path.join(datadir, 'd_score_DOWN.csv'))
+    # d_score_down = score_gene_pairs_diff(Gene_Pairs_down, 'd_score_DOWN.csv')
+    d_score_up = score_gene_pairs(Gene_Pairs_up, os.path.join(datadir, 'd_score_UP.csv'))
+    # d_score_up = score_gene_pairs_diff(Gene_Pairs_up, 'd_score_UP.csv')
+    PES = (d_score_up - d_score_down).sort_values(by='score', ascending=False)
+    PES.to_csv(os.path.join(datadir,'d_score.csv'))
+    print(d_score_down)
+    print(d_score_up)
+    print(PES)
+    PES.reset_index(drop=False, inplace=True)
+    # PES = pd.read_csv(os.path.join(datadir,'d_score.csv'))
+
+    # last step: output drugs based on d score
+    drug_score = drug_repurposing(drugDB, PES)
+    drugScoreFile = os.path.join(datadir, 'drug_score.csv')
+    drug_score.to_csv(drugScoreFile, index=False)
+    print('Gene D score mapped to repurposing drugs saved to\n{}'.format(drugScoreFile))
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ docker/pipelines/py/proteomics_gen.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import getopt
+import time
+import unidecode
+import pandas as pd
+import numpy as np
+# from bioservices import BioDBNet
+from transcriptomic_gen import *
+from project import configs
+
+# Load Proteomics
+def load_proteomics_data(datafilename):
+    data_sheet = list(range(1)) # first 5 sheets
+    dataFullPath = os.path.join(configs.rootdir, 'data', datafilename)
+    if os.path.isfile(dataFullPath):
+        fulldata = pd.read_excel(dataFullPath, sheet_name=data_sheet, header=0)
+    else:
+        print("Error: file not found: {}".format(dataFullPath))
+        return None
+    proteomics_data = fulldata[0]
+
+    # Preprocess data, drop na, duplicate ';' in Gene names,
+    proteomics_data['Gene names'] = proteomics_data['Gene names'].astype(str)
+    proteomics_data.dropna(subset=['Gene names'],inplace=True)
+    pluralnames = proteomics_data[proteomics_data['Gene names'].str.contains(';')==True]
+    for idx, row in pluralnames.iterrows():
+        # print('{}: {}'.format(idx, row['Gene names']))
+        names = row['Gene names'].split(';')
+        rows = []
+        for name in names:
+            rowcopy = row.copy()
+            rowcopy['Gene names'] = name
+            rows.append(rowcopy)
+        proteomics_data.drop(index=idx,inplace=True)
+        proteomics_data.append(rows, ignore_index=True)
+
+    proteomics_data.rename(columns={'Gene names':'Gene Symbol'}, inplace=True)
+    return proteomics_data
+
+
+def load_supplementary_data(suppfilename):
+    suppFullPath = os.path.join(configs.rootdir, 'data', suppfilename)
+    supp_sheet = ['Proteomics']
+    supplements = pd.read_excel(suppFullPath, sheet_name=supp_sheet, header=0)
+    Proteomics = supplements['Proteomics']
+    return Proteomics
+
+
+def load_gene_symbol_map(gene_symbols, filename = 'proteomics_entrez_map.csv'):
+    filepath = os.path.join(configs.rootdir, 'data', 'proteomics_entrez_map.csv')
+    if os.path.isfile(filepath):
+        sym2id = pd.read_csv(filepath, index_col='Gene Symbol')
+    else:
+        sym2id = fetch_entrez_gene_id(gene_symbols, input_db='Gene Symbol')
+        sym2id.loc[sym2id['Gene ID'] == '-', ['Gene ID']] = np.nan
+        sym2id.to_csv(filepath, index_label='Gene Symbol')
+    return sym2id[~sym2id.index.duplicated()]
+
+
+def save_proteomics_tests(Proteomics, proteomics_data):
+    tests = []
+    files = []
+    datas = []
+    for test in list(Proteomics):
+        if test == 'Statistics':
+            break
+        cols = Proteomics[test].dropna().tolist()
+        testdata = proteomics_data.loc[:, cols]
+        # Logical Calculation
+        thresholds = testdata.quantile(0.75, axis=0)
+        testbool = pd.DataFrame(0, columns=list(testdata), index=testdata.index)
+        for col in list(testdata):
+            testbool.loc[testdata[col] > thresholds[col], [col]] = 1
+
+        testdata['pos'] = (testdata > 0).sum(axis=1) / testdata.count(axis=1)
+        testdata['0.5'] = 0
+        testdata.loc[(testdata['pos'] > 0.5), ['0.5']] = 1
+        testdata['top 0.25'] = 0
+        testdata.loc[testbool.any(axis=1), 'top 0.25'] = 1
+
+        test = unidecode.unidecode(test)
+        output_path = os.path.join(configs.rootdir, 'data', 'Proteomics_{}.csv'.format(test.strip()))
+        testdata.to_csv(output_path, index_label='ENTREZ_GENE_ID')
+        print('Test Data Saved to {}'.format(output_path))
+        tests.append(test.strip())
+        files.append(output_path)
+        datas.append(testdata)
+
+    proteomics_dict = dict(zip(tests, files))
+    testdata_dict = dict(zip(tests, datas))
+    return proteomics_dict, testdata_dict
+
+
+# Read data from csv files
+def load_proteomics_tests(Proteomics):
+    tests = []
+    datas = []
+    for test in list(Proteomics):
+        if test == 'Statistics':
+            break
+        test = unidecode.unidecode(test)
+        output_path = os.path.join(configs.rootdir, 'data', 'Proteomics_{}.csv'.format(test.strip()))
+        testdata = pd.read_csv(output_path, index_col='ENTREZ_GENE_ID')
+        print('Test Data Load From {}'.format(output_path))
+        tests.append(test.strip().replace('ï', 'i'))
+        datas.append(testdata)
+
+    proteomics_dict = dict(zip(tests, datas))
+    return proteomics_dict
+
+
+def main(argv):
+    datafilename = 'ni.3693-S5.xlsx'
+    suppfilename = 'Supplementary Data 1.xlsx'
+    try:
+        opts, args = getopt.getopt(argv, "hd:s:", ["datafile=", "suppfile="])
+    except getopt.GetoptError:
+        print('python3 proteomics_gen.py -d <data file> -s <supplementary data file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('python3 proteomics_gen.py -d <data file> -s <supplementary data file>')
+            sys.exit()
+        elif opt in ("-d", "--datafile"):
+            datafilename = arg
+        elif opt in ("-s", "--suppfile"):
+            suppfilename = arg
+    print('Data file is "{}"'.format(datafilename))
+    print('Supplementary Data file is "{}"'.format(suppfilename))
+
+    proteomics_data = load_proteomics_data(datafilename)
+    Proteomics = load_supplementary_data(suppfilename)
+    sym2id = load_gene_symbol_map(gene_symbols=proteomics_data['Gene Symbol'].tolist(),
+                                  filename='proteomics_entrez_map.csv')
+
+    # map gene symbol to ENTREZ_GENE_ID
+    proteomics_data.dropna(subset=['Gene Symbol'], inplace=True)
+    proteomics_data.drop(columns=['Majority protein IDs'], inplace=True)
+    proteomics_data = proteomics_data.groupby(['Gene Symbol']).agg('max')
+    # proteomics_data.set_index('Gene Symbol', inplace=True)
+    proteomics_data['ENTREZ_GENE_ID'] = sym2id['Gene ID']
+    proteomics_data.dropna(subset=['ENTREZ_GENE_ID'], inplace=True)
+    proteomics_data.set_index('ENTREZ_GENE_ID', inplace=True)
+    prote_data_filepath = os.path.join(configs.rootdir, 'data', 'proteomics_data.csv')
+    if not os.path.isfile(prote_data_filepath):
+        proteomics_data.to_csv(prote_data_filepath, index_label='ENTREZ_GENE_ID')
+
+    # save proteomics data by test
+    proteomics_dict, testdata_dict = save_proteomics_tests(Proteomics, proteomics_data)
+    return True
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
