81cbd5db5a1e25f6ad20bd84496e62b39694a18f
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2021-05-10 15:51:13 -0400

    added Bulk RNA-seq capabilities

--- docker/pipelines/py/merge_xomics.py
+++ docker/pipelines/py/merge_xomics.py
@@ -11,39 +11,163 @@ import json
 from project import configs
 from transcriptomic_gen import *
 from proteomics_gen import *
+from bulk_gen import *
 from create_tissue_specific_model import splitGeneExpressionData
 
 # input parameters
-def merge_xomics(transcript_file='transcriptomics_data_inputs.xlsx', prote_file='proteomics_data_inputs.xlsx'):
+def merge_xomics(transcript_file=None,
+                 prote_file=None,
+                 bulk_file=None):
+
     transcriptomics_dict= load_transcriptomics_tests(filename = transcript_file)
-    Proteomics = load_supplementary_data(prote_file)
+    Proteomics = load_prot_supplementary_data(prote_file)
     proteomics_dict = load_proteomics_tests(Proteomics)
-
+    print(proteomics_dict)
+    Bulk = load_bulk_supplementary_data(bulk_file)
+    bulk_dict = load_bulk_tests(Bulk)
+    print(bulk_dict)
     files_dict = dict()
-    keys1 = proteomics_dict.keys()
-    keys2 = transcriptomics_dict.keys()
-    tests = set(keys1).intersection(set(keys2))
+
+    #keys1 = proteomics_dict.keys()
+    #keys2 = transcriptomics_dict.keys()
+    #keys3 = bulk_dict.keys()
+
+    #tests = set(keys2).intersection(set(keys3))
+    if prote_file and transcript_file and bulk_file:
+        keys1 = proteomics_dict.keys()
+        keys2 = transcriptomics_dict.keys()
+        keys3 = bulk_dict.keys()
+        tests = set(keys1).intersection(set(keys2))
+        tests = tests.intersection(set(keys3))
+        tests2_1 = (set(keys1).union(set(keys2))).difference(tests)
+        tests2_2 = (set(keys1).union(set(keys3))).difference(tests)
+        tests2_3 = (set(keys2).union(set(keys3))).difference(tests)
+        tests2 = test2_1.union(tests2_2)
+        tests2 = tests2.union(tests2_3)
+    elif prote_file and transcript_file:
+        keys1 = proteomics_dict.keys()
+        keys2 = transcriptomics_dict.keys()
+        tests = set(keys1).intersection(set(keys2))
+        tests2 = (set(keys1).union(set(keys2))).difference(tests)
+    elif prote_file and bulk_file:
+        keys1 = proteomics_dict.keys()
+        print(keys1)
+        keys3 = bulk_dict.keys()
+        print(keys3)
+        tests = set(keys1).intersection(set(keys3))
+        print(tests)
+        tests2 = (set(keys1).union(set(keys3))).difference(tests)
+        print(tests2)
+    elif prote_file:
+        keys1 = proteomics_dict.keys()
+        tests = set(keys1)
+        tests2 = tests
+    elif transcript_file and bulk_file:
+        keys2 = transcriptomics_dict.keys()
+        keys3 = bulk_dict.keys()
+        tests = set(keys2).intersection(keys3)
+        tests2 = (set(keys2).union(set(keys3))).difference(tests)
+    elif transcript_file:
+        keys2 = transcriptomics_dict.keys()
+        tests = set(keys2)
+        tests2 = tests
+    elif bulk_file:
+        keys3 = bulk_dict.keys()
+        tests = set(keys3)
+        tests2 = tests
+    else:
+        getopt.GetoptError
+        print("No files given")
+        sys.exit(2)
+    #tests = set(keys1).intersection(set(keys2))
+    print("keys1 generated")
+    merge_data = None
     for test in tests:
-        prote_data = proteomics_dict[test].loc[:, ['0.5', 'top 0.25']]
-        prote_data.rename(columns={'0.5': 'prote_0.5', 'top 0.25': 'prote_top'}, inplace=True)
+        print(test)
+        if prote_file:
+            prote_data = proteomics_dict[test].loc[:, ['expressed', 'top']]
+            prote_data.rename(columns={'expressed': 'prote_exp',
+                                       'top': 'prote_top'}, inplace=True)
+            #prote_data = mergeLogicalTable(prote_data)
+            print(prote_data)
+            test = unidecode.unidecode(test)
+            try:
+                if not merge_data:
+                    merge_data = prote_data
+            except:
+                merge_data = None
 
-        trans_data = transcriptomics_dict[test].loc[:, ['0.5', '0.9']]
-        trans_data.rename(columns={'0.5': 'trans_0.5', '0.9': 'trans_top'}, inplace=True)
+        if transcript_file:
+            trans_data = transcriptomics_dict[test].loc[:, ['expressed', 'top']]
+            trans_data.rename(columns={'expressed': 'trans_exp',
+                                       'top': 'trans_top'}, inplace=True)
 
-        print(trans_data.shape)
-        print(prote_data.shape)
-        test = unidecode.unidecode(test)
+            test = unidecode.unidecode(test)
+            try:
+                if not merge_data:
+                    merge_data = trans_data
+            except:
+                merge_data = merge_data.join(trans_data, how='outer')
 
-        merge_data = prote_data.join(trans_data, how='outer')
+        if bulk_file:
+            bulk_data = bulk_dict[test].loc[:, ['expressed', 'top']]
+            bulk_data.rename(columns={'expressed': 'bulk_exp',
+                                      'top': 'bulk_top'}, inplace=True)
+            #bulk_data = mergeLogicalTable(bulk_data)
+            print(bulk_data)
+            test = unidecode.unidecode(test)
+            try:
+                if not merge_data:
+                    merge_data = bulk_data
+            except:
+                merge_data = merge_data.join(bulk_data, how='outer')
+
+        print(merge_data)
         merge_data = mergeLogicalTable(merge_data)
-        merge_data['Express'] = 0
-        merge_data.loc[merge_data[['prote_0.5', 'trans_0.5']].sum(axis=1) == 2, 'Express'] = 1
-        merge_data.loc[merge_data[['prote_top', 'trans_top']].sum(axis=1) > 0, 'Express'] = 1
+        #test = unidecode.unidecode(test)
+        print(merge_data)
+        if prote_file and transcript_file and bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp', 'trans_exp', 'bulk_exp']].sum(axis=1) == 3, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top', 'trans_top', 'bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+
+        elif prote_file and transcript_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp', 'trans_exp']].sum(axis=1) == 2, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top', 'trans_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif prote_file and bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp', 'bulk_exp']].sum(axis=1) == 2, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top', 'bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif prote_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp']].sum(axis=1) == 1, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif trasnscript_file and bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['trans_exp', 'bulk_exp']].sum(axis=1) == 2, 'Express'] = 1
+            merge_data.loc[merge_data[['trans_top', 'bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif transcript_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['trans_exp']].sum(axis=1) == 1, 'Express'] = 1
+        elif bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['bulk_exp']].sum(axis=1) == 1, 'Express'] = 1
+            merge_data.loc[merge_data[['bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+
+
+        #merge_data = prote_data.join(trans_data, how='outer')
+        #merge_data = mergeLogicalTable(merge_data)
+        #merge_data['Express'] = 0
+        #merge_data.loc[merge_data[['prote_0.5', 'trans_0.5']].sum(axis=1) == 2, 'Express'] = 1
+        #merge_data.loc[merge_data[['prote_top', 'trans_top']].sum(axis=1) > 0, 'Express'] = 1
+
         filepath = os.path.join(configs.rootdir, 'data', 'merged_{}.csv'.format(test))
         merge_data.to_csv(filepath, index_label='ENTREZ_GENE_ID')
 
         filepath = os.path.join(configs.rootdir, 'data', 'GeneExpression_{}_Merged.csv'.format(test))
         merge_data.reset_index(drop=False, inplace=True)
+
         splitEntrez = splitGeneExpressionData(merge_data)
         splitEntrez.rename(columns={'Gene':'ENTREZ_GENE_ID', 'Data':'Express'}, inplace=True)
         splitEntrez.to_csv(filepath, index_label='ENTREZ_GENE_ID')
@@ -52,8 +176,77 @@ def merge_xomics(transcript_file='transcriptomics_data_inputs.xlsx', prote_file=
         print('{}: save to {}\n'.format(test, filepath))
 
     # Those only with transcriptomics or proteomics
-    tests2 = (set(keys1).union(set(keys2))).difference(tests)
-    for test in tests2:
+    #tests2 = (set(keys1).union(set(keys2))).difference(tests)
+    #for test in tests2:
+    while False:
+        test = unidecode.unidecode(test)
+        if prote_file:
+            prote_data = proteomics_dict[test].loc[:, ['expressed', 'top']]
+            prote_data.rename(columns={'expressed': 'prote_exp',
+                                       'top': 'prote_top'}, inplace=True)
+            print(prote_data.shape)
+            test = unidecode.unidecode(test)
+            if not merge_data:
+                merge_data = prote_data
+
+        if transcript_file:
+            trans_data = transcriptomics_dict[test].loc[:, ['expressed', 'top']]
+            trans_data.rename(columns={'expressed': 'trans_exp',
+                                       'top': 'trans_top'}, inplace=True)
+            print(trans_data.shape)
+            test = unidecode.unidecode(test)
+            if not merge_data:
+                merge_data = trans_data
+            else:
+                merge_data = merge_data.join(trans_data, how='outer')
+
+        if bulk_file:
+            bulk_data = bulk_dict[test].loc[:, ['expressed', 'top']]
+            bulk_data.rename(columns={'expressed': 'bulk_exp',
+                                      'top': 'bulk_top'}, inplace=True)
+            print(bulk_data.shape)
+            test = unidecode.unidecode(test)
+            if not merge_data:
+                merge_data = bulk_data
+            else:
+                merge_data = merge_data.join(bulk_data, how='outer')
+
+        #test = unidecode.unidecode(test)
+        if prote_file and transcript_file and bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp', 'trans_exp', 'bulk_exp']].sum(axis=1) == 3, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top', 'trans_top', 'bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif prote_file and transcript_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp', 'trans_exp']].sum(axis=1) == 2, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top', 'trans_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif prote_file and bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp', 'bulk_exp']].sum(axis=1) == 2, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top', 'bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif prote_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['prote_exp']].sum(axis=1) == 1, 'Express'] = 1
+            merge_data.loc[merge_data[['prote_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif trasnscript_file and bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['trans_exp', 'bulk_exp']].sum(axis=1) == 2, 'Express'] = 1
+            merge_data.loc[merge_data[['trans_top', 'bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+        elif transcript_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['trans_exp']].sum(axis=1) == 1, 'Express'] = 1
+        elif bulk_file:
+            merge_data['Express'] = 0
+            merge_data.loc[merge_data[['bulk_exp']].sum(axis=1) == 1, 'Express'] = 1
+            merge_data.loc[merge_data[['bulk_top']].sum(axis=1) > 0, 'Express'] = 1
+        else:
+            print("Unknown Error")
+            return None
+
+        merge_data = mergeLogicalTable(merge_data)
+
+
+        old = '''
         if test in keys1:
             merge_data = proteomics_dict[test].loc[:, ['0.5', 'top 0.25']]
             merge_data.rename(columns={'0.5': '0.5', 'top 0.25': 'top'}, inplace=True)
@@ -65,11 +258,13 @@ def merge_xomics(transcript_file='transcriptomics_data_inputs.xlsx', prote_file=
             return None
 
         test = unidecode.unidecode(test)
+
         merge_data = mergeLogicalTable(merge_data)
         merge_data['Express'] = 0
         merge_data.loc[merge_data[['0.5', 'top']].sum(axis=1) > 0, 'Express'] = 1
         filepath = os.path.join(configs.rootdir, 'data', 'merged_{}.csv'.format(test))
         merge_data.to_csv(filepath, index_label='ENTREZ_GENE_ID')
+        '''
 
         filepath = os.path.join(configs.rootdir, 'data', 'GeneExpression_{}_Merged.csv'.format(test))
         merge_data.reset_index(drop=False, inplace=True)
@@ -83,10 +278,11 @@ def merge_xomics(transcript_file='transcriptomics_data_inputs.xlsx', prote_file=
     return files_dict
 
 def main(argv):
-    transfile = 'transcriptomics_data_inputs.xlsx'
-    protefile = 'proteomics_data_input.xlsx'
+    transfile = None
+    protefile = None
+    bulkfile = None
     try:
-        opts, args = getopt.getopt(argv, "ht:p:", ["transfile=", "protefile="])
+        opts, args = getopt.getopt(argv, "ht:p:b:", ["transfile=", "protefile=", "bulkfile="])
     except getopt.GetoptError:
         print('merge_xomics.py -t <transfile> -p <protefile>')
         sys.exit(2)
@@ -98,12 +294,16 @@ def main(argv):
             transfile = arg
         elif opt in ("-p", "--protefile"):
             protefile = arg
+        elif opt in ('-b', "--bulkfile"):
+            bulkfile = arg
     print('Transcriptomics file is "{}"'.format(transfile))
     print('Proteomics file is "{}"'.format(protefile))
+    print('Bulk RNA-seq file is "{}"'.format(bulkfile))
     files_dict = merge_xomics(transcript_file=transfile,
-                              prote_file=protefile)
+                              prote_file=protefile,
+                              bulk_file=bulkfile)
     print(files_dict)
-    files_json = os.path.join(configs.datadir, 'step1_results_files.json')
+    files_json = os.path.join(configs.rootdir, 'data', 'step1_results_files.json')
     with open(files_json, 'w') as fp:
         json.dump(files_dict, fp)
 
--- docker/pipelines/py/proteomics_gen.py
+++ docker/pipelines/py/proteomics_gen.py
@@ -14,6 +14,8 @@ from project import configs
 # Load Proteomics
 def load_proteomics_data(datafilename):
     data_sheet = list(range(1)) # first 5 sheets
+    # CHANGED BC I CANT USE DOCKER DONT FORGET TO CHANGE BACK
+    #dataFullPath = "G:/GitHub/New Folder/MADRID/docker/pipelines/py/data/"+datafilename
     dataFullPath = os.path.join(configs.rootdir, 'data', datafilename)
     if os.path.isfile(dataFullPath):
         fulldata = pd.read_excel(dataFullPath, sheet_name=data_sheet, header=0)
@@ -41,15 +43,23 @@ def load_proteomics_data(datafilename):
     return proteomics_data
 
 
-def load_supplementary_data(suppfilename):
+def load_prot_supplementary_data(suppfilename):
+    if not suppfilename:
+        #suppfilename = "dummy_proteomics_config.xlsx"
+        return None
+    # CHANGED BC I CANT USE DOCKER DONT FORGET TO CHANGE BACK
+    #suppFullPath = "G:/GitHub/New Folder/MADRID/docker/pipelines/py/data/"+suppfilename
     suppFullPath = os.path.join(configs.rootdir, 'data', suppfilename)
     supp_sheet = ['Proteomics']
     supplements = pd.read_excel(suppFullPath, sheet_name=supp_sheet, header=0)
     Proteomics = supplements['Proteomics']
+    print(Proteomics)
     return Proteomics
 
 
 def load_gene_symbol_map(gene_symbols, filename = 'proteomics_entrez_map.csv'):
+    # CHANGED BC I CANT USE DOCKER DONT FORGET TO CHANGE BACK
+    #filepath = "G:/GitHub/New Folder/MADRID/docker/pipelines/py/data/proteomics_entrez_map.csv"
     filepath = os.path.join(configs.rootdir, 'data', 'proteomics_entrez_map.csv')
     if os.path.isfile(filepath):
         sym2id = pd.read_csv(filepath, index_col='Gene Symbol')
@@ -60,7 +70,7 @@ def load_gene_symbol_map(gene_symbols, filename = 'proteomics_entrez_map.csv'):
     return sym2id[~sym2id.index.duplicated()]
 
 
-def save_proteomics_tests(Proteomics, proteomics_data):
+def save_proteomics_tests(Proteomics, proteomics_data, expr_prop, percentile):
     tests = []
     files = []
     datas = []
@@ -70,18 +80,25 @@ def save_proteomics_tests(Proteomics, proteomics_data):
         cols = Proteomics[test].dropna().tolist()
         testdata = proteomics_data.loc[:, cols]
         # Logical Calculation
-        thresholds = testdata.quantile(0.75, axis=0)
+        thresholds = testdata.quantile(1-percentile, axis=0)
         testbool = pd.DataFrame(0, columns=list(testdata), index=testdata.index)
         for col in list(testdata):
             testbool.loc[testdata[col] > thresholds[col], [col]] = 1
 
         testdata['pos'] = (testdata > 0).sum(axis=1) / testdata.count(axis=1)
-        testdata['0.5'] = 0
-        testdata.loc[(testdata['pos'] > 0.5), ['0.5']] = 1
-        testdata['top 0.25'] = 0
-        testdata.loc[testbool.any(axis=1), 'top 0.25'] = 1
+        ##testdata[str(expr_prop)] = 0
+        testdata['expressed'] = 0
+        #testdata.loc[(testdata['pos'] > expr_prop), [str(expr_prop)]] = 1
+        testdata.loc[(testdata['pos'] > expr_prop), ['expressed']] = 1
+        #testdata['top '+str(percentile)] = 0
+        testdata['top'] = 0
+        #testdata.loc[testbool.any(axis=1), 'top '+str(percentile)] = 1
+        #TODO: change this to expr_prop
+        testdata.loc[testbool.any(axis=1), 'top'] = 1
 
         test = unidecode.unidecode(test)
+        # CHANGED BC I CANT USE DOCKER DONT FORGET TO CHANGE BACK
+        #output_path = 'G:/GitHub/New Folder/MADRID/docker/pipelines/py/data' + 'Proteomics_{}.csv'.format(test.strip()))
         output_path = os.path.join(configs.rootdir, 'data', 'Proteomics_{}.csv'.format(test.strip()))
         testdata.to_csv(output_path, index_label='ENTREZ_GENE_ID')
         print('Test Data Saved to {}'.format(output_path))
@@ -96,14 +113,24 @@ def save_proteomics_tests(Proteomics, proteomics_data):
 
 # Read data from csv files
 def load_proteomics_tests(Proteomics):
+    try:
+        if Proteomics==None:
+            return None
+    except:
+        print("proteomics exists")
+
     tests = []
     datas = []
     for test in list(Proteomics):
+        print(test)
         if test == 'Statistics':
             break
         test = unidecode.unidecode(test)
+        # CHANGED BC I CANT USE DOCKER DONT FORGET TO CHANGE BACK
+        #output_path = 'G:/GitHub/New Folder/MADRID/docker/pipelines/py/data' + 'Proteomics_{}.csv'.format(test.strip()))
         output_path = os.path.join(configs.rootdir, 'data', 'Proteomics_{}.csv'.format(test.strip()))
         testdata = pd.read_csv(output_path, index_col='ENTREZ_GENE_ID')
+        
         print('Test Data Load From {}'.format(output_path))
         tests.append(test.strip().replace('ï', 'i'))
         datas.append(testdata)
@@ -115,8 +142,11 @@ def load_proteomics_tests(Proteomics):
 def main(argv):
     datafilename = 'ProteomicsDataMatrix.xlsx'
     suppfilename = 'proteomics_data_inputs.xlsx'
+    expr_prop = 0.5
+    percentile = 0.25
+
     try:
-        opts, args = getopt.getopt(argv, "hd:s:", ["datafile=", "suppfile="])
+        opts, args = getopt.getopt(argv, "hd:s:e:p:", ["datafile=", "suppfile=", "expr_proportion=", "percentile="])
     except getopt.GetoptError:
         print('python3 proteomics_gen.py -d <data file> -s <supplementary data file>')
         sys.exit(2)
@@ -128,11 +158,15 @@ def main(argv):
             datafilename = arg
         elif opt in ("-s", "--suppfile"):
             suppfilename = arg
+        elif opt in ("-e"):
+            expr_prop = float(arg)
+        elif opt in ("-p"):
+            percentile = float(arg)/100
     print('Data file is "{}"'.format(datafilename))
     print('Supplementary Data file is "{}"'.format(suppfilename))
 
     proteomics_data = load_proteomics_data(datafilename)
-    Proteomics = load_supplementary_data(suppfilename)
+    Proteomics = load_prot_supplementary_data(suppfilename)
     sym2id = load_gene_symbol_map(gene_symbols=proteomics_data['Gene Symbol'].tolist(),
                                   filename='proteomics_entrez_map.csv')
 
@@ -144,12 +178,14 @@ def main(argv):
     proteomics_data['ENTREZ_GENE_ID'] = sym2id['Gene ID']
     proteomics_data.dropna(subset=['ENTREZ_GENE_ID'], inplace=True)
     proteomics_data.set_index('ENTREZ_GENE_ID', inplace=True)
+    # CHANGED BC I CANT USE DOCKER DONT FORGET TO CHANGE BACK
+    #prote_data_filepath = 'G:/GitHub/New Folder/MADRID/docker/pipelines/py/data/proteomics_data.csv'
     prote_data_filepath = os.path.join(configs.rootdir, 'data', 'proteomics_data.csv')
     if not os.path.isfile(prote_data_filepath):
         proteomics_data.to_csv(prote_data_filepath, index_label='ENTREZ_GENE_ID')
 
     # save proteomics data by test
-    proteomics_dict, testdata_dict = save_proteomics_tests(Proteomics, proteomics_data)
+    proteomics_dict, testdata_dict = save_proteomics_tests(Proteomics, proteomics_data, expr_prop, percentile)
     return True
 
 
