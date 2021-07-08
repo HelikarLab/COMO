8d8ab1e32bc5fb612fa49f945bccfa78bb8d8db0
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2021-07-08 14:17:47 -0400

    setup jupyter

--- docker/setup_jupyter.sh
+++ docker/setup_jupyter.sh
@@ -1,7 +1,9 @@
-python3 -m pip --no-cache-dir install jupyter notebook
-python3 -m pip --no-cache-dir install jupyterlab
-python3 -m pip --no-cache-dir install bokeh
-python3 -m pip --no-cache-dir install ipywidgets
-jupyter nbextension enable --py --sys-prefix widgetsnbextension
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-python3 -m pip --no-cache-dir install jupyterhub
+mkdir /home/"${NB_USER}"/work
+mkdir /home/"${NB_USER}"/.jupyter
+mkdir /home/"${NB_USER}"/.local
+echo "cacert=/etc/ssl/certs/ca-certificates.crt" > /home/"${NB_USER}"/.curlrc
+echo "c.NotebookApp.ip = '0.0.0.0'" >> /home/"${NB_USER}"/.jupyter/jupyter_notebook_config.py
+python3 -m venv /home/"${NB_USER}"/py3_env
+echo "source /home/${NB_USER}/py3_env/bin/activate" >> /home/"${NB_USER}"/.bashrc
+echo "echo Python virtual environment activated. Write \"deactivate\" to exit it." >> /home/"${NB_USER}"/.bash
+chown -R "${NB_USER}" /home/"${NB_USER}"/
