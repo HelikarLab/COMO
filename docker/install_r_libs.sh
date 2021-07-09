d6e409d9841bba64b82ff2a58f9b673a151f7511
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2021-07-09 16:37:42 -0400

    update global variable NB_USER in rlib install to jupyteruser

--- docker/install_r_libs.sh
+++ docker/install_r_libs.sh
@@ -5,4 +5,4 @@ mkdir /home/"${NB_USER}"/rlibs
 #R -e 'BiocManager::install("limma", dep=TRUE, ask=FALSE)'
 #R -e 'BiocManager::install("hgu133acdf", dep=TRUE, ask=FALSE)'
 #R -e 'install.packages(c("rzmq","repr","IRkernel","IRdisplay"), repos = c("http://irkernel.github.io/", getOption("repos")), type = "source")'
-R -e 'install.packages(c("tidyverse", "sjmisc"), dependencies=TRUE, repos="'"${CRAN_MIRROR}"'", lib="/home/"${NB_USER}"/rlibs")'
+R -e 'install.packages(c("tidyverse", "sjmisc"), dependencies=TRUE, repos="'"${CRAN_MIRROR}"'", lib="/home/jupyteruser/rlibs")'
