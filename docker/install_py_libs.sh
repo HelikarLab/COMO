478030dd368ec5168f67a0d02695c6c125d2a1fe
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2021-07-13 13:12:18 -0400

    fix bioservices installation

--- docker/install_py_libs.sh
+++ docker/install_py_libs.sh
@@ -2,6 +2,7 @@ python3 -m pip --no-cache-dir install pip --upgrade
 python3 -m pip --no-cache-dir install setuptools --upgrade
 python3 -m pip --no-cache-dir install wheel --upgrade
 python3 -m pip --no-cache-dir install \
+	bioservices \
 	cobra \
 	GEOparse \
 	lxml \
@@ -11,10 +12,6 @@ python3 -m pip --no-cache-dir install \
 	SQLAlchemy \
 	framed \
 	xlrd \
-	beautifulsoup4 \
-	SOAPpy-py3 \
-	suds-py3 \
-	bioservices \
 	cobamp \
 	unidecode \
 	troppo
