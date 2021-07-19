51e833121399ff58c100114373b7d75d47d373e3
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2021-07-19 17:28:25 -0400

    Update install_r_libs.sh

--- docker/install_r_libs.sh
+++ docker/install_r_libs.sh
@@ -1,5 +1,6 @@
 mkdir /home/"${NB_USER}"/rlibs
 chown -R "${NB_USER}" /home/"${NB_USER}"/rlibs
+chown -R "/usr/local/lib/R/site-library"
 R -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")'
 R -e 'BiocManager::install("affy", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
 R -e 'BiocManager::install("agilp", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
@@ -10,3 +11,13 @@ R -e 'install.packages(c("tidyverse", "sjmisc"), dependencies=TRUE, repos="'"${C
 R -e 'BiocManager::install("genefilter", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
 R -e 'BiocManager::install("biomaRt", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
 R -e 'BiocManager::install("edgeR", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
+#R -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")'
+#R -e 'BiocManager::install("affy", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
+#R -e 'BiocManager::install("agilp", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
+#R -e 'BiocManager::install("limma", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
+#R -e 'BiocManager::install("hgu133acdf", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
+#R -e 'install.packages(c("rzmq","repr","IRkernel","IRdisplay"), repos = c("http://irkernel.github.io/", getOption("repos")), type = "source", lib="/home/jupyteruser/rlibs")'
+#R -e 'install.packages(c("tidyverse", "sjmisc"), dependencies=TRUE, repos="'"${CRAN_MIRROR}"'", lib="/home/jupyteruser/rlibs")'
+#R -e 'BiocManager::install("genefilter", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
+#R -e 'BiocManager::install("biomaRt", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
+#R -e 'BiocManager::install("edgeR", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
