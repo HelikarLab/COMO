0582af5020cc6de922cd8d93f47744c5b2145a8d
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2021-07-20 16:33:39 -0400

    chown change after Rlib downloads

--- docker/install_r_libs.sh
+++ docker/install_r_libs.sh
@@ -11,6 +11,7 @@ R -e 'install.packages(c("tidyverse", "sjmisc"), dependencies=TRUE, repos="'"${C
 R -e 'BiocManager::install("genefilter", dep=TRUE, ask=FALSE)'
 R -e 'BiocManager::install("biomaRt", dep=TRUE, ask=FALSE)'
 R -e 'BiocManager::install("edgeR", dep=TRUE, ask=FALSE)'
+chown -R /usr/local/lib/R/site-library
 #R -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")'
 #R -e 'BiocManager::install("affy", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
 #R -e 'BiocManager::install("agilp", dep=TRUE, ask=FALSE, lib="/home/jupyteruser/rlibs")'
