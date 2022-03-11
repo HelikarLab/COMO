311c8d94438e7bec321f9c8e03eb4d1b168aff48
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-03-11 13:32:02 -0500

    [fix] dupicate devtools instal, ropo for zfpkm

--- install_r_libs.sh
+++ install_r_libs.sh
@@ -2,19 +2,14 @@ R -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("Bi
 R -e 'install.packages("devtools", dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
 R -e 'devtools::install_version("locfit", version="1.5-9.4", dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
 R -e 'BiocManager::install("edgeR", dep=TRUE, ask=FALSE)'
-R -e 'devtools::install_github("babessell1/zFPKM", dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
-R -e 'install.packages("devtools", dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
+R -e 'devtools::install_github("babessell1/zFPKM", dependencies=TRUE)'
 R -e 'BiocManager::install("affy", dep=TRUE, ask=FALSE)'
 R -e 'BiocManager::install("agilp", dep=TRUE, ask=FALSE)'
 R -e 'BiocManager::install("limma", dep=TRUE, ask=FALSE)'
 R -e 'BiocManager::install("hgu133acdf", dep=TRUE, ask=FALSE)'
 R -e 'install.packages(c("rzmq","repr","IRkernel","IRdisplay"), repos = c("http://irkernel.github.io/", getOption("repos")), type = "source")'
-R -e 'install.packages(c("tidyverse", "sjmisc", "openxlsx", "countToFPKM"), dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
 R -e 'BiocManager::install("genefilter", dep=TRUE, ask=FALSE)'
-#R -e 'BiocManager::install("biomaRt", dep=TRUE, ask=FALSE)'
-#R -e 'BiocManager::install("edgeR", dep=TRUE, ask=FALSE)'
 R -e 'BiocManager::install("DESeq2", dep=TRUE, ask=FALSE)'
-#R -e 'BiocManager::install("zFPKM", dep=TRUE, ask=FALSE)'
 
 
 #mkdir /home/"${NB_USER}"/rlibs
