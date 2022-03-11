3bd8ab6025ad3faf72252a9e2f7fa45ae45d4d9c
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-03-10 20:23:56 -0500

    Update install_r_libs.sh

--- install_r_libs.sh
+++ install_r_libs.sh
@@ -1,6 +1,6 @@
 R -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")'
-R -e 'install.packages("devtools", dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
 R -e 'BiocManager::install("edgeR", dep=TRUE, ask=FALSE)'
+R -e 'install.packages("devtools", dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
 R -e 'devtools::install_github("babessell1/zFPKM", dependencies=TRUE, repos="'"${CRAN_MIRROR}"'")'
 R -e 'BiocManager::install("affy", dep=TRUE, ask=FALSE)'
 R -e 'BiocManager::install("agilp", dep=TRUE, ask=FALSE)'
