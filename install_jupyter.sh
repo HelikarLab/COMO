cb384ab2e4a2cd496b794ed96a6c884b32abddc0
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-04-03 19:17:15 -0400

    [docker] install escher lab extension

--- install_jupyter.sh
+++ install_jupyter.sh
@@ -4,4 +4,5 @@ python3 -m pip --no-cache-dir install bokeh
 python3 -m pip --no-cache-dir install ipywidgets
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install escher
 python3 -m pip --no-cache-dir install jupyterhub
