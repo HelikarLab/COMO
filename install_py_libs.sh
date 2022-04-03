32d039ce54c2dcf8190dbd6f60cf674ac5ebb6a0
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-04-03 13:50:57 -0400

    [fix] git install git:// to https://

--- install_py_libs.sh
+++ install_py_libs.sh
@@ -2,7 +2,7 @@ python3 -m pip --no-cache-dir install pip --upgrade
 python3 -m pip --no-cache-dir install setuptools==58.0.1 --upgrade
 python3 -m pip --no-cache-dir install wheel --upgrade
 python3 -m pip --no-cache-dir install \
-	git+git://github.com/cokelaer/bioservices.git@master#egg=bioservices \
+	git+https://github.com/cokelaer/bioservices.git@master#egg=bioservices \
 	argparse \
 	cobra \
 	GEOparse \
@@ -15,7 +15,7 @@ python3 -m pip --no-cache-dir install \
 	framed \
 	xlrd \
 	openpyxl \
-	git+git://github.com/babessell1/cobamp.git@master#egg=cobamp \
+	git+https://github.com/babessell1/cobamp.git@master#egg=cobamp \
 	unidecode \
 	troppo \
 	escher
