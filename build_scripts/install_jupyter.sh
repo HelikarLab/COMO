af11a417ae0baaaf49594138ef7b7ded4efdf0e6
Josh Loecker <joshloecker@icloud.com>
2022-04-10 17:20:35 -0500

    [fix] Install jupyter labextensions in one line

--- build_scripts/install_jupyter.sh
+++ build_scripts/install_jupyter.sh
@@ -1,5 +1,4 @@
 python3 -m pip --no-cache-dir install jupyter notebook jupyterlab bokeh ipywidgets
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install escher
+jupyter labextension install @jupyter-widgets/jupyterlab-manager escher
 python3 -m pip --no-cache-dir install jupyterhub
