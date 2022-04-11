a4c5619d1849ede598208bbad76bdb37155dbd3a
Josh Loecker <joshloecker@icloud.com>
2022-04-11 11:33:53 -0500

    [feature] Use a single jupyter/python setup file     Remove the need for virtual environments, as the libraries installed here are separated by docker

--- build_scripts/install_jupyter.sh
+++ build_scripts/install_jupyter.sh
@@ -1,3 +1,5 @@
+source /home/"${NB_USER}"/py3_env/bin/activate
+
 python3 -m pip --no-cache-dir install jupyter notebook jupyterlab bokeh ipywidgets
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter labextension install @jupyter-widgets/jupyterlab-manager escher
