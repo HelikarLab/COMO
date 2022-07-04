6a6b65fd0a3a6d5ed4729bc128825a83480e80d8
Josh Loecker <joshloecker@icloud.com>
2022-07-04 14:41:38 -0500

    Added init to be able to import files for unit testing Use raw string in regex ("\d")

--- work/py/tests/test_FileInformation.py
+++ work/py/tests/test_FileInformation.py
@@ -1,24 +1,9 @@
 import os
 import sys
-
+from pathlib import Path
 # Add parent directory to path, allows us to import the "project.py" file from the parent directory
 # From: https://stackoverflow.com/a/30536516/13885200
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from proteomics.FileInformation import FileInformation
 
 
-class TestFileInformation:
-    """
-    cell_type: str,
-    download_url: str = None,
-    study: int | str = None,
-    raw_path: Path = None,
-    intensity_csv: Path = None,
-    mzml_path: Path = None,
-    sqt_path: Path = None,
-    file_size: int = None,
-    """
-    information = FileInformation(
-        cell_type="naiveB",
-        download_url="ftp://ftp.pride.ebi.ac.uk/pride-archive/2022/02/PXD026172",
-    )
