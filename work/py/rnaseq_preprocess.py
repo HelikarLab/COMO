92e1dff08792aa37a3872092300cc9c83377cbdc
Josh Loecker <joshloecker@icloud.com>
2022-08-09 16:05:15 -0500

    Enable async gathering when accessing BioDBNet, greatly increases db2db conversion

--- work/py/rnaseq_preprocess.py
+++ work/py/rnaseq_preprocess.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import asyncio
 from bioservices import BioDBNet
 import pandas as pd
 from project import configs
@@ -20,17 +21,114 @@ f.close()
 generate_counts_matrix_io = SignatureTranslatedAnonymousPackage(string, 'generate_counts_matrix_io')
 
 
+async def _async_fetch_gene_info(biodbnet: BioDBNet, **kwargs) -> pd.DataFrame:
+    input_db: str = kwargs["input_db"]
+    output_db: list[str] = kwargs["output_db"]
+    input_values: list[str] = kwargs["input_values"]
+    taxon_id: int = kwargs["taxon_id"]
+    delay: int = kwargs["delay"]
+    event_loop = kwargs["event_loop"]
+
+    # print(f"Getting {current_index}:{current_index + len(input_values)}")
+    database_convert = await event_loop.run_in_executor(
+        None,  # Defaults to ThreadPoolExecutor, uses threads instead of processes. No need to modify
+        biodbnet.db2db,  # The function to call
+        input_db,       # The following are arguments passed to the function
+        output_db,
+        input_values,
+        taxon_id
+    )
+
+    # If the above db2db conversion didn't work, try again until it does
+    while not isinstance(database_convert, pd.DataFrame):
+        print(f"\nToo many requests to BioDBNet, waiting {delay} seconds and trying again.")
+        await asyncio.sleep(delay)
+        database_convert = await event_loop.run_in_executor(
+            None,  # Defaults to ThreadPoolExecutor, uses threads instead of processes. No need to modify
+            _async_fetch_gene_info,  # The function to call
+            biodbnet,
+            kwargs
+        )
+
+    return database_convert
+
+
+def fetch_gene_info_refactor(
+        input_values: list[str],
+        input_db: str = "Ensembl Gene ID",
+        output_db: list[str] = None,
+        taxon_id: int = 9606,
+        delay: int = 5
+):
+    """
+    This function returns a dataframe with important gene information for future operations in MADRID.
+
+    Fetch gene information from BioDBNet
+    :param input_values: A list of genes in "input_db" format
+    :param input_db: The input database to use (default: "Ensembl Gene ID")
+    :param output_db: The output format to use (default: ["Gene Symbol", "Gene ID", "Chromosomal Location"])
+    :param delay: The delay in seconds to wait before trying again if bioDBnet is busy (default: 15)
+    :param taxon_id: The taxon ID to use (default: 9606)
+
+    :return: A dataframe with specified columns as "output_db" (Default is HUGO symbol, Entrez ID, and chromosome start and end positions)
+    """
+    if output_db is None:
+        output_db: list[str] = ["Gene Symbol", "Gene ID", "Chromosomal Location"]
+
+    biodbnet = BioDBNet()
+    dataframe_maps: pd.DataFrame = pd.DataFrame([], columns=output_db)
+    dataframe_maps.index.name = input_db
+
+    if taxon_id == 9606:
+        batch_len = 500
+    else:
+        batch_len = 300
+
+    # Create a list of tasks to be awaited
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    async_tasks = []
+    for i in range(0, len(input_values), batch_len):
+        # Define an upper range of values to take from input_values
+        upper_range = min(i + batch_len, len(input_values))
+
+        # Create a task to await
+        async_tasks.append(
+            _async_fetch_gene_info(
+                biodbnet,
+                input_db=input_db,
+                output_db=output_db,
+                input_values=input_values[i:upper_range],
+                taxon_id=taxon_id,
+                delay=delay,
+                event_loop=event_loop
+            )
+        )
+
+    # Await all the tasks, must use [0] to get results. [1] is an empty set.
+    database_convert = event_loop.run_until_complete(asyncio.wait(async_tasks))[0]
+    event_loop.close()
+
+    # Loop over database_convert to concat them into dataframe_maps
+    for df in database_convert:
+        dataframe_maps = pd.concat([dataframe_maps, df.result()], sort=False)
+
+    return dataframe_maps
+
+
 def fetch_gene_info(input_values, input_db="Ensembl Gene ID",
                     output_db=["Gene Symbol", "Gene ID", "Chromosomal Location"],
                     delay=15, taxon_id=9606):
     """
+    THIS FUNCTION IS DEPRECATED. USE fetch_gene_info_refactor INSTEAD.
+
     Returns a dataframe with important gene information for future operations in MADRID.
 
     Fetch gene information from BioDbNet, takes 'input_values' (genes) in format of 'input_db' (default, Ensembl) and
     ond returns dataframe with specified columns as 'output_db' (default is HUGO symbol, Entrez ID, and chromosome
     chromosomal start and end positions).
     """
-    s = BioDBNet()
+    biodbnet = BioDBNet()
     df_maps = pd.DataFrame([], columns=output_db)
     df_maps.index.name = input_db
     i = 0
@@ -41,7 +139,7 @@ def fetch_gene_info(input_values, input_db="Ensembl Gene ID",
         # TODO: Make this output more user-readable
         # It outputs many lines due to the large number of genes to retrieve
         print(f"retrieve {i}:{upper_range}")
-        df_test = s.db2db(input_db, output_db, input_values[i:upper_range], taxon_id)
+        df_test = biodbnet.db2db(input_db, output_db, input_values[i:upper_range], taxon_id)
         if isinstance(df_test, pd.DataFrame):
             df_maps = pd.concat([df_maps, df_test], sort=False)
         elif df_test == '414':
@@ -154,7 +252,7 @@ def create_config_df(context_name):
 
         # Get preparation method
         if len(prep_glob) < 1:
-            print(f"\nNo prep file found for {label}, assuming 'total' as in Total RNA library preparation")
+            print(f"No prep file found for {label}, assuming 'total' as in Total RNA library preparation")
             prep = "total"
         elif len(prep_glob) > 1:
             print(f"\nMultiple matching prep files for {label}, make sure there is only one copy for each "
@@ -258,7 +356,9 @@ def create_gene_info_file(matrix_file_list, form, taxon_id):
 
     output_db = ['Ensembl Gene ID', 'Gene Symbol', 'Gene ID', 'Chromosomal Location']
     output_db.remove(form)
-    gene_info = fetch_gene_info(genes, input_db=form, output_db=output_db, taxon_id=taxon_id)
+
+    print(f"Gathering {len(genes)} genes. Please wait...")
+    gene_info = fetch_gene_info_refactor(input_values=genes, input_db=form, output_db=output_db, taxon_id=taxon_id)
     gene_info['start_position'] = gene_info['Chromosomal Location'].str.extract("chr_start: (\d+)")
     gene_info['end_position'] = gene_info['Chromosomal Location'].str.extract("chr_end: (\d+)")
     gene_info.index.rename("ensembl_gene_id", inplace=True)
@@ -266,8 +366,6 @@ def create_gene_info_file(matrix_file_list, form, taxon_id):
     gene_info.drop(['Chromosomal Location'], axis=1, inplace=True)
     gene_info.to_csv(gene_info_file)
     print(f"Gene Info file written at '{gene_info_file}'")
-
-    return
 
 
 def handle_context_batch(context_names, mode, form, taxon_id, provided_matrix_file):
