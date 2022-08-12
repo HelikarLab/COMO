3cd933c8955e1a34a8b1cfa4e82ed05e276429c4
Josh Loecker <joshloecker@icloud.com>
2022-08-12 11:09:59 -0500

    Added a test for project.py

--- work/py/project.py
+++ work/py/project.py
@@ -8,7 +8,6 @@ import time
 class Configs:
     def __init__(self, projectdir):
         self.rootdir = projectdir
-        # self.rootdir = "G:/GitHub/MADRID/docker/pipelines/"
         self.datadir = os.path.join(projectdir, "data")
         self.configdir = os.path.join(projectdir, "data", "config_sheets")
         self.outputdir = os.path.join(projectdir, "output")
@@ -19,13 +18,12 @@ class Configs:
 currentdir = os.getcwd()
 dirlist = currentdir.split("/")
 
-# Find the "pipelines" directory
+# Find the "work" directory
 split_index = 1
 for directory in dirlist:
-    if directory != "work":
-        split_index += 1
-    else:
-        break
+    if directory == "work":
+        break  # Exit the loop when we find the "work" directory
+    split_index += 1  # Otherwise increment the index
 
 # Unpack items in dirlist
 # From: https://stackoverflow.com/questions/14826888
