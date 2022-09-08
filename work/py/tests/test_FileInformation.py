c5f6215c4bed41d2e05be669d0641ab8b56ad321
Josh Loecker <joshloecker@icloud.com>
2022-09-08 13:16:05 -0500

    Test project.py
    This includes tests to ensure the project.py file maintains the correct folders

--- work/py/tests/test_FileInformation.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from pathlib import Path
-# Add parent directory to path, allows us to import the "project.py" file from the parent directory
-# From: https://stackoverflow.com/a/30536516/13885200
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from proteomics.FileInformation import FileInformation
-
-
