eb594e325ec7603f4dab23dcaf5acce94aff6703
Josh Loecker <joshloecker@icloud.com>
2022-09-09 01:23:27 -0500

    Fixed external conflict

--- main/py/async_bioservices/database_convert.py
+++ main/py/async_bioservices/database_convert.py
@@ -6,36 +6,47 @@ from .output_database import OutputDatabase
 from.taxon_ids import TaxonIDs
 
 
-async def _async_fetch_info(biodbnet: BioDBNet, **kwargs):
-    input_db: str = kwargs["input_db"]
-    output_db: list[str] = kwargs["output_db"]
-    input_values: list[str] = kwargs["input_values"]
-    taxon_id: int = kwargs["taxon_id"]
-    delay: int = kwargs["delay"]
-    event_loop = kwargs["event_loop"]
+async def _async_fetch_info(
+        biodbnet: BioDBNet,
+        input_values: list[str],
+        input_db: str,
+        output_db: list[str],
+        taxon_id: TaxonIDs | int,
+        delay: int
+):
 
-    # print(f"Getting {current_index}:{current_index + len(input_values)}")
-    database_convert = await event_loop.run_in_executor(
-        None,  # Defaults to ThreadPoolExecutor, uses threads instead of processes. No need to modify
+    if isinstance(taxon_id, TaxonIDs):
+        taxon_id_value = taxon_id.value
+    else:
+        taxon_id_value = taxon_id
+
+    database_convert = await asyncio.to_thread(
         biodbnet.db2db,  # The function to call
         input_db,       # The following are arguments passed to the function
         output_db,
         input_values,
-        taxon_id
+        taxon_id_value
     )
 
     # If the above db2db conversion didn't work, try again until it does
     while not isinstance(database_convert, pd.DataFrame):
         print(f"\nToo many requests to BioDBNet, waiting {delay} seconds and trying again.")
         await asyncio.sleep(delay)
-        database_convert = await event_loop.run_in_executor(
-            None,  # Defaults to ThreadPoolExecutor, uses threads instead of processes. No need to modify
+        database_convert = await asyncio.to_thread(
             _async_fetch_info,  # The function to call
             biodbnet,
-            kwargs
+            input_values,
+            input_db,
+            output_db,
+            taxon_id,
+            delay
         )
 
     return database_convert
+
+
+async def _async_fetch_info_wrapper(coroutines: list):
+    return await asyncio.gather(*coroutines)
 
 
 def fetch_gene_info(
@@ -57,16 +68,19 @@ def fetch_gene_info(
 
     :return: A dataframe with specified columns as "output_db" (Default is HUGO symbol, Entrez ID, and chromosome start and end positions)
     """
+    input_db = input_db.value
     if output_db is None:
         output_db: list[str] = [
             OutputDatabase.GENE_SYMBOL.value,
             OutputDatabase.GENE_ID.value,
             OutputDatabase.CHROMOSOMAL_LOCATION.value
         ]
+    else:
+        output_db: list[str] = [i.value for i in output_db]
 
     biodbnet = BioDBNet()
     dataframe_maps: pd.DataFrame = pd.DataFrame([], columns=output_db)
-    dataframe_maps.index.name = input_db.value
+    dataframe_maps.index.name = input_db
 
     if taxon_id == TaxonIDs.HUMAN:
         batch_len = 500
@@ -74,32 +88,27 @@ def fetch_gene_info(
         batch_len = 300
 
     # Create a list of tasks to be awaited
-    event_loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(event_loop)
-    async_tasks = []
+    async_coroutines = []
     for i in range(0, len(input_values), batch_len):
         # Define an upper range of values to take from input_values
         upper_range = min(i + batch_len, len(input_values))
 
-        # Create a task to await
-        async_tasks.append(
+        async_coroutines.append(
             _async_fetch_info(
-                biodbnet,
+                biodbnet=biodbnet,
+                input_values=input_values[i:upper_range],
                 input_db=input_db,
                 output_db=output_db,
-                input_values=input_values[i:upper_range],
                 taxon_id=taxon_id,
-                delay=delay,
-                event_loop=event_loop
+                delay=delay
             )
         )
 
     # Await all the tasks, must use [0] to get completed results. [1] is a set of pending tasks (i.e., empty).
-    database_convert = event_loop.run_until_complete(asyncio.wait(async_tasks))[0]
-    event_loop.close()  # Close the event loop to free resources
+    database_convert = asyncio.run(_async_fetch_info_wrapper(async_coroutines))
 
     # Loop over database_convert to concat them into dataframe_maps
     for df in database_convert:
-        dataframe_maps = pd.concat([dataframe_maps, df.result()], sort=False)
+        dataframe_maps = pd.concat([dataframe_maps, df], sort=False)
 
     return dataframe_maps
