7fc24913561de156d2cd67a78b9583d42e90e05c
Josh Loecker <joshloecker@icloud.com>
2022-09-09 00:21:55 -0500

    Remove bioservices import

--- main/py/tests/test_async_bioservices.py
+++ main/py/tests/test_async_bioservices.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import pandas as pd
-import bioservices
 
 # Add parent directory to path, allows us to import the "project.py" file from the parent directory
 # From: https://stackoverflow.com/a/30536516/13885200
