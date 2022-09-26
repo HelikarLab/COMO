832ce64b39318b9c82bc0393d222a83008512451
Josh Loecker <joshloecker@icloud.com>
2022-09-26 16:25:41 -0500

    Do not import __init__ modules if running under pytest

--- main/py/async_bioservices/__init__.py
+++ main/py/async_bioservices/__init__.py
@@ -1,4 +1,9 @@
-import database_convert
-import input_database
-import output_database
-import taxon_ids
+import sys
+
+# Only need to import the modules if NOT running under PyTest
+# from: https://stackoverflow.com/questions/25188119
+if "pytest" not in sys.modules:
+    import database_convert
+    import input_database
+    import output_database
+    import taxon_ids
