18c604dea14de7476da75fe4673d993a34347e23
Josh Loecker <joshloecker@icloud.com>
2022-09-26 16:56:10 -0500

    New command line list parsing (#53)

--- main/py/proteomics/__init__.py
+++ main/py/proteomics/__init__.py
@@ -1,4 +1,9 @@
-# from . import Crux
-# from . import FileInformation
-# from . import FTPManager
-# from . import proteomics_preprocess
+import sys
+
+# Only need to import the modules if NOT running under PyTest
+# from: https://stackoverflow.com/questions/25188119
+if "pytest" not in sys.modules:
+    import Crux
+    import FileInformation
+    import FTPManager
+    import proteomics_preprocess
