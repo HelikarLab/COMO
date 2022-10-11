ebb75e5eebd73711b1c39a442231085414658912
Josh Loecker <joshloecker@icloud.com>
2022-10-10 22:44:07 -0500

    Remove unnecessary flow-control statements

--- main/py/async_bioservices/__init__.py
+++ main/py/async_bioservices/__init__.py
@@ -1,19 +1,4 @@
-import sys
-
 from . import database_convert
 from . import input_database
 from . import output_database
 from . import taxon_ids
-
-# Only need to import the modules if NOT running under PyTest
-# from: https://stackoverflow.com/questions/25188119
-#if "pytest" not in sys.modules:
-#    from . import database_convert
-#    from . import input_database
-#    from . import output_database
-#    from . import taxon_ids
-#else:
-#    from . import database_convert
-#    from . import input_database
-#    from . import output_database
-#    from . import taxon_ids
