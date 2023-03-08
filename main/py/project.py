849da00b565f6e01dea7638742b07803606c6756
Josh Loecker <joshloecker@icloud.com>
2023-03-08 14:22:08 -0600

    Create dataclasses for items listed under config.toml

--- main/py/project.py
+++ main/py/project.py
@@ -92,29 +92,105 @@ class Configs:
         self.pydir = os.path.join(projectdir, "py")
        
         self._toml_data: dict = self._read_from_toml()
-        self.general = self._toml_data["general"]
-        self.rnaseq_preprocess = self._toml_data["rnaseq_preprocess"]
-        self.rna_seq_generation = self._toml_data["rna_seq_generation"]
-        self.proteomics_analysis = self._toml_data["proteomics_analysis"]
-        self.merge_xomics = self._toml_data["merge_xomics"]
-        self.model_creation = self._toml_data["model_creation"]
-        self.disease_analysis = self._toml_data["disease_analysis"]
-        self.drug_repurposing = self._toml_data["drug_repurposing"]
-        self.about = self._toml_data["about"]
         
-        self._add_date_to_about()
+        self.general: general = self._get_general()
+        self.rnaseq_preprocess: rnaseq_preprocess = self._get_rnaseq_preprocess()
+        self.rna_seq_generation: rna_seq_generation = self._get_rna_seq_generation()
+        self.proteomics_analysis: proteomics_analysis = self._get_proteomics_analysis()
+        self.merge_xomics: merge_xomics = self._get_merge_xomics()
+        self.model_creation: model_creation = self._get_model_creation()
+        self.disease_analysis: disease_analysis = self._get_disease_analysis()
+        self.drug_repurposing: drug_repurposing = self._get_drug_repurposing()
+        self.about: about = self._get_about()
+
+    def _get_general(self) -> general:
+        data: general = general(
+            taxon_id=self._toml_data["general"]["taxon_id"],
+            context_names=self._toml_data["general"]["context_names"],
+        )
+        return data
+
+    def _get_rnaseq_preprocess(self) -> rnaseq_preprocess:
+        data: rnaseq_preprocess = rnaseq_preprocess(
+            create_counts_matrix=self._toml_data["rnaseq_preprocess"]["create_counts_matrix"],
+            gene_format=self._toml_data["rnaseq_preprocess"]["gene_format"],
+            preprocess_mode=self._toml_data["rnaseq_preprocess"]["preprocess_mode"],
+        )
+        return data
+
+    def _get_rna_seq_generation(self) -> rna_seq_generation:
+        data: rna_seq_generation = rna_seq_generation(
+            trnaseq_config_file=self._toml_data["rna_seq_generation"]["trnaseq_config_file"],
+            mrnaseq_config_file=self._toml_data["rna_seq_generation"]["mrnaseq_config_file"],
+            technique=self._toml_data["rna_seq_generation"]["technique"],
+            rep_ratio=self._toml_data["rna_seq_generation"]["rep_ratio"],
+            group_ratio=self._toml_data["rna_seq_generation"]["group_ratio"],
+            rep_ratio_h=self._toml_data["rna_seq_generation"]["rep_ratio_h"],
+            group_ratio_h=self._toml_data["rna_seq_generation"]["group_ratio_h"],
+            quantile=self._toml_data["rna_seq_generation"]["quantile"],
+            min_zfpkm=self._toml_data["rna_seq_generation"]["min_zfpkm"],
+        )
+        return data
+
+    def _get_proteomics_analysis(self) -> proteomics_analysis:
+        data: proteomics_analysis = proteomics_analysis(
+            proteomics_config_file=self._toml_data["proteomics_analysis"]["proteomics_config_file"],
+            rep_ratio=self._toml_data["proteomics_analysis"]["rep_ratio"],
+            batch_ratio=self._toml_data["proteomics_analysis"]["batch_ratio"],
+            high_rep_ratio=self._toml_data["proteomics_analysis"]["high_rep_ratio"],
+            high_batch_ratio=self._toml_data["proteomics_analysis"]["high_batch_ratio"],
+            quantile=self._toml_data["proteomics_analysis"]["quantile"],
+        )
+        return data
+
+    def _get_merge_xomics(self) -> merge_xomics:
+        data: merge_xomics = merge_xomics(
+            expression_requirement=self._toml_data["merge_xomics"]["expression_requirement"],
+            requirement_adjust=self._toml_data["merge_xomics"]["requirement_adjust"],
+            total_rna_weight=self._toml_data["merge_xomics"]["total_rna_weight"],
+            mrna_weight=self._toml_data["merge_xomics"]["mrna_weight"],
+            single_cell_weight=self._toml_data["merge_xomics"]["single_cell_weight"],
+            proteomics_weight=self._toml_data["merge_xomics"]["proteomics_weight"],
+        )
+        return data
+
+    def _get_model_creation(self) -> model_creation:
+        data: model_creation = model_creation(
+            low_threshold=self._toml_data["model_creation"]["low_threshold"],
+            high_threshold=self._toml_data["model_creation"]["high_threshold"],
+            output_filetypes=self._toml_data["model_creation"]["output_filetypes"],
+            objective_dict=self._toml_data["model_creation"]["objective_dict"],
+            general_model_file=self._toml_data["model_creation"]["general_model_file"],
+            solver=self._toml_data["model_creation"]["solver"],
+            boundary_reactions_filename=self._toml_data["model_creation"]["boundary_reactions_filename"],
+            force_reactions_filename=self._toml_data["model_creation"]["force_reactions_filename"],
+            exclude_reactions_filename=self._toml_data["model_creation"]["exclude_reactions_filename"],
+            recon_algorithms=self._toml_data["model_creation"]["recon_algorithms"],
+        )
+        return data
+
+    def _get_disease_analysis(self) -> disease_analysis:
+        data: disease_analysis = disease_analysis(
+            data_source=self._toml_data["disease_analysis"]["data_source"],
+            disease_names=self._toml_data["disease_analysis"]["disease_names"],
+        )
+        return data
+
+    def _get_drug_repurposing(self) -> drug_repurposing:
+        data: drug_repurposing = drug_repurposing(
+            sovler=self._toml_data["drug_repurposing"]["sovler"],
+            drug_raw_file=self._toml_data["drug_repurposing"]["drug_raw_file"],
+        )
+        return data
+
+    def _get_about(self) -> about:
+        return about()
 
     def _read_from_toml(self):
         toml_file: str = os.path.join(self.rootdir, "config.toml")
         with open(toml_file, "rb") as i_stream:
             data = tomllib.load(i_stream)
         return data
-    
-    def _add_date_to_about(self):
-        """
-        This function will add the current date in the format of "Month Day, Year" to the self.about dictionary
-        """
-        self.about["date"] = datetime.now().strftime("%B %d, %Y")
     
 
 current_dir = os.getcwd()
