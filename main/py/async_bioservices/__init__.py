31743daa08f5df4e1e6b56b71443576b0edadacd
Josh Loecker <joshloecker@icloud.com>
2023-06-20 17:12:18 -0500

    Check that input database is not included in output database, formatting fixes

--- main/py/async_bioservices/__init__.py
+++ main/py/async_bioservices/__init__.py
@@ -1,4 +1,8 @@
-from . import async_bioservices
-from . import input_database
-from . import output_database
-from . import taxon_ids
+from .async_bioservices import fetch_gene_info
+from .input_database import InputDatabase
+from .output_database import OutputDatabase
+from .taxon_ids import TaxonIDs
+# from . import async_bioservices
+# from . import input_database
+# from . import output_database
+# from . import taxon_ids
