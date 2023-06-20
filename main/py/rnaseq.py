df3b04f02f1ef6502db6bc8c934ea0a0d251abaf
Josh Loecker <joshloecker@icloud.com>
2023-06-20 17:11:08 -0500

    Added variables to SampleMetrics class Require rnanorm to perform CPM, TPM, etc.; issue filed on GitHub over problems

--- main/py/rnaseq.py
+++ main/py/rnaseq.py
@@ -1,14 +1,19 @@
 """
 This is a python implementation of the rnaseq.R file
 """
+import os
 import re
+import rnanorm
+import sklearn
+import tempfile
 import numpy as np
 import pandas as pd
 from enum import Enum
 from pathlib import Path
 from typing import Any, Callable, Union
 from dataclasses import dataclass, field
-import sklearn
+
+from async_bioservices import fetch_gene_info, InputDatabase, OutputDatabase, TaxonIDs
 
 
 class NormalizationTechnique(Enum):
@@ -41,7 +46,10 @@ class FilterSettings:
 
 @dataclass
 class SampleMetricItem:
+    context_name: str
     study_number: str
+    species: str
+    gtf_file: Path
     layout: str
     sample_names: str
     count_matrix: pd.DataFrame
@@ -62,7 +70,10 @@ class SampleMetricItem:
 
 @dataclass
 class SampleMetrics:
+    context_name: str = None
     study_number: str = None
+    species: str = None
+    gtf_file: Path = None
     layout: list[str] = field(default_factory=list[str])
     sample_names: list[str] = field(default_factory=list[str])
     count_matrix: pd.DataFrame = None
@@ -78,7 +89,10 @@ class SampleMetrics:
     
     def __getitem__(self, index: Union[int, slice]) -> SampleMetricItem:
         return SampleMetricItem(
+            context_name=self.context_name,
             study_number=self.study_number,
+            species=self.species,
+            gtf_file=self.gtf_file,
             layout=self.layout[index].lower(),
             sample_names=self.sample_names[index],
             count_matrix=self.count_matrix,
@@ -259,14 +273,15 @@ def create_sample_metrics(
     
     counts_matrix = counts_matrix.loc[gene_info["entrezgene_id"] != "-"]  # remove unnamed genes
     gene_info = gene_info[gene_info["entrezgene_id"] != "-"]  # remove unnamed genes
-    genes = gene_info["entrezgene_id"]  # get gene names
+    genes = gene_info[["entrezgene_id", "ensembl_gene_id"]]  # get gene names
+    genes = genes.sort_values("ensembl_gene_id", ignore_index=True)
     
     # Remove version numbers from ensembl id
     for i in range(len(genes)):
-        row = genes.iloc[i]
+        row = genes.iloc[i, 0]
         if re.search(r"\.", row):
             modified = row.split(".")[0]
-            genes[i] = modified
+            genes.iloc[i, 0] = modified
     
     sample_metrics: dict[str, SampleMetrics] = {}
     groups = config_object["Group"].unique()
@@ -281,7 +296,7 @@ def create_sample_metrics(
         
         if entry in counts_matrix.columns:
             # These values will be added to the sample_matrix object
-            new_sample_matrix = counts_matrix[entry]
+            new_sample_matrix = counts_matrix[[entry, "genes"]]
             new_fragment_length = config_object["FragmentLength"][i]
             new_layout = config_object["Layout"][i]
             new_sample_name = entry
@@ -291,27 +306,37 @@ def create_sample_metrics(
                 new_fragment_length = 0
             
             # Update values
+            sample_metrics[group].context_name = context_name
             sample_metrics[group].layout.append(new_layout)
             sample_metrics[group].sample_names.append(new_sample_name)
             sample_metrics[group].fragment_lengths.append(new_fragment_length)
-            sample_metrics[group].count_matrix = pd.concat(
-                [
-                    sample_metrics[group].count_matrix,
-                    new_sample_matrix
-                ],
-                axis=1
+            
+            # Merge sample_metrics[group].count_matrix with new_sample_matrix on `genes`
+            sample_metrics[group].count_matrix = pd.merge(
+                sample_metrics[group].count_matrix,
+                new_sample_matrix,
+                left_on="ensembl_gene_id",
+                right_on="genes",
+                how="outer"
             )
+            
+            # Remove `entry` rows that are nan
+            # Remove `genes` column, it is duplicated in `ensembl_gene_id`
+            sample_metrics[group].count_matrix = sample_metrics[group].count_matrix[
+                ~sample_metrics[group].count_matrix[entry].isna()
+            ]
+            sample_metrics[group].count_matrix = sample_metrics[group].count_matrix.drop(columns=["genes"])
         else:
             print(f"WARNING: {entry} not found in count matrix {str(counts_matrix_file)}")
     
     for group in groups:
-        sample_matrix = sample_metrics[group].count_matrix
-        sample_matrix = sample_matrix.iloc[:, 1:]
-        sample_matrix = sample_matrix.apply(pd.to_numeric)
-        sample_matrix.columns = sample_metrics[group].sample_names  # set column names to sample names
+        # Remove rows with nan entrezgene_id
+        sample_metrics[group].count_matrix = sample_metrics[group].count_matrix[
+            ~sample_metrics[group].count_matrix["entrezgene_id"].isna()
+        ]
         
-        sample_metrics[group].count_matrix = sample_matrix  # update counts matrix
-        sample_metrics[group].num_samples = sample_matrix.shape[1]  # set number of samples
+        sample_metrics[group].count_matrix.rename(columns={"ensembl_gene_id": "genes"}, inplace=True)
+        sample_metrics[group].num_samples = sample_metrics[group].count_matrix.shape[1]  # set number of samples
         sample_metrics[group].entrez_ids = gene_info["entrezgene_id"].astype(str)  # store entrez ids
         sample_metrics[group].gene_size = gene_info["size"]  # store gene size
         sample_metrics[group].study_number = group
@@ -437,6 +462,7 @@ def cpm_filter(
 
 
 def tpm_filter(
+    gtf_directorypath: Path,
     sample_metrics: dict[str, SampleMetrics],
     filter_settings: FilterSettings,
     context_name: str,
@@ -444,14 +470,28 @@ def tpm_filter(
     output_directory: Path,
     per_million_scaling: int = 1e6
 ) -> dict[str, SampleMetrics]:
-    return CalculateTPM(
-        sample_metrics=sample_metrics,
-        filter_settings=filter_settings,
-        prep_method=prep_method,
-        context_name=context_name,
-        output_directory=output_directory,
-        per_million_scaling=per_million_scaling
-    ).sample_metrics
+    """
+    This function will use rnanorm's TPM function to calculate TPM values for each gene in each sample
+    """
+    for key in sample_metrics.keys():
+        current_metric: SampleMetrics = sample_metrics[key]
+        calculation_matrix = current_metric.count_matrix.filter(like=context_name)
+        gtf_file: Path = gtf_directorypath / f"{key}.gtf"
+        
+        print(calculation_matrix)
+        print(current_metric.gene_size)
+        
+        tpm = rnanorm.TPM(gtf=gtf_file)  # gene_lengths=current_metric.gene_size)
+        tpm.fit_transform(calculation_matrix)
+    
+    # return CalculateTPM(
+    #     sample_metrics=sample_metrics,
+    #     filter_settings=filter_settings,
+    #     prep_method=prep_method,
+    #     context_name=context_name,
+    #     output_directory=output_directory,
+    #     per_million_scaling=per_million_scaling
+    # ).sample_metrics
 
 
 def zfpkm_filter():
@@ -486,6 +526,95 @@ def filter_counts(
     else:
         raise ValueError(
             f"Invalid technique: {filter_settings.technique}. Must be one of ['cpm', 'zfpkm', 'quantile', 'umi']")
+
+
+def create_gtf_file(
+    sample_metrics: dict[str, SampleMetrics],
+    context_name: str
+) -> Path:
+    """
+    In order to use rnanroam TPM and FPKM functions, a GTF file is required.
+    This function will take the count matrix and create a GTF file with the
+    async_bioservices library
+
+    The following items are required:
+        - seqname - name of the chromosome or scaffold; chromosome names can be given with or without the 'chr' prefix. Important note: the seqname must be one used within Ensembl, i.e. a standard chromosome name or an Ensembl identifier such as a scaffold ID, without any additional content such as species or assembly. See the example GFF output below.
+        - source - name of the program that generated this feature, or the data source (database or project name)
+        - feature - feature type name, e.g. Gene, Variation, Similarity
+        - start - Start position* of the feature, with sequence numbering starting at 1.
+        - end - End position* of the feature, with sequence numbering starting at 1.
+        - score - A floating point value.
+        - strand - defined as + (forward) or - (reverse).
+        - frame - One of '0', '1' or '2'. '0' indicates that the first base of the feature is the first base of a codon, '1' that the second base is the first base of a codon, and so on..
+        - attribute - A semicolon-separated list of tag-value pairs, providing additional information about each feature.
+
+    Each item can be obtained from the following source in the OutputDatabase:
+        - seqname: CHROMOSOMAL_LOCATION
+        - source: "." literal
+        - feature: "gene" literal
+        - start: CHROMOSOMAL_LOCATION
+        - end: CHROMOSOMAL_LOCATION
+        - score: "." literal
+        - strand: ENSEMBL_GENE_INFO (-1 is reverse, 1 is forward)
+        - frame: "." literal
+        - attribute: f"gene_id {ENSEMBL_GENE_ID};"
+        
+    :param sample_metrics: A dictionary of SampleMetrics objects
+    :return: A directory path containing GTF files
+    """
+    
+    seq_name_pattern = re.compile(r"\[chr:\s*([^]]+)\]")
+    start_pattern = re.compile(r"\[chr_start:\s*([^]]+)\]")
+    end_pattern = re.compile(r"\[chr_end:\s*([^]]+)\]")
+    strand_pattern = re.compile(r"\[Chr_Strand:\s*(-?1)\]")
+    
+    temp_directory = os.path.join(tempfile.tempdir, "COMO", context_name)
+    os.makedirs(temp_directory, exist_ok=True)
+    
+    for key in sample_metrics.keys():
+        current_metric: SampleMetrics = sample_metrics[key]
+        
+        conversion: pd.DataFrame = fetch_gene_info(
+            input_values=current_metric.count_matrix["genes"].tolist(),
+            input_db=InputDatabase.ENSEMBL_GENE_ID,
+            output_db=[
+                OutputDatabase.CHROMOSOMAL_LOCATION,
+                OutputDatabase.ENSEMBL_GENE_INFO
+            ],
+            taxon_id=TaxonIDs.HOMO_SAPIENS
+        )
+        
+        # Set the index to a column called "ensembl_gene_id"
+        conversion["ensembl_gene_id"] = conversion.index
+        conversion.reset_index(inplace=True, drop=True)
+        conversion.rename(columns={
+            "Chromosomal Location": "chromosome_location",
+            "Ensembl Gene Info": "ensembl_gene_info"
+        }, inplace=True)
+        
+        gtf_df: pd.DataFrame = pd.DataFrame(
+            data={
+                "seqname": conversion["chromosome_location"].apply(lambda row: seq_name_pattern.search(row).group(1)),
+                "source": ".",
+                "feature": "gene",
+                "start": conversion["chromosome_location"].apply(lambda row: start_pattern.search(row).group(1)),
+                "end": conversion["chromosome_location"].apply(lambda row: end_pattern.search(row).group(1)),
+                "score": ".",
+                "strand": conversion["ensembl_gene_info"].apply(lambda row: strand_pattern.search(row).group(1)),
+                "frame": ".",
+                "attribute": [f"gene_id {ensembl_gene_id};" for ensembl_gene_id in conversion["ensembl_gene_id"]]
+            }
+        )
+        gtf_df.sort_values("seqname", inplace=True, ignore_index=True)
+        
+        gtf_df.to_csv(
+            os.path.join(temp_directory, f"{key}.gtf"),
+            sep="\t",
+            header=False,
+            index=False
+        )
+    
+    return Path(temp_directory)
 
 
 def main(
@@ -527,21 +656,16 @@ def main(
         info_file=info_file,
         context_name=context_name,
     )
-    # calculate_fpkm(sample_metrics)
     
-    result = filter_counts(
+    gtf_directory: Path = create_gtf_file(sample_metrics=sample_metrics, context_name=context_name)
+    tpm_filter(
+        gtf_directorypath=gtf_directory,
         sample_metrics=sample_metrics,
         filter_settings=filter_settings,
         context_name=context_name,
         prep_method=prep,
+        output_directory=out_file.parent / context_name
     )
-    print(result["S3"].count_matrix)
-    
-    # expressed_genes: list = []
-    # top_genes: list = []
-    # for key in sample_metrics.keys():
-    #     expressed_genes.append(sample_metrics[i]["Entrez"])
-    #     top_genes.append(sample_metrics[i]["Entrez_hc"])
 
 
 if __name__ == "__main__":
