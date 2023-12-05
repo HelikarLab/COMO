ad16537cb7deb53dcfdc854e0c94776f5203c687
Josh Loecker <joshloecker@icloud.com>
2023-12-05 09:27:48 -0600

    Initial implementation of creating a "configuration.toml" file

--- main/src/project.py
+++ main/src/project.py
@@ -1,14 +1,48 @@
 import os
+import toml
+import subprocess
+from pathlib import Path
 
 
 # find project root dir
 class Configs:
     def __init__(self, projectdir: str) -> None:
-        self.rootdir = projectdir
-        self.datadir = os.path.join(projectdir, "data")
-        self.configdir = os.path.join(projectdir, "data", "config_sheets")
-        self.outputdir = os.path.join(projectdir, "output")
-        self.pydir = os.path.join(projectdir, "src")
+        self.root_dir = projectdir
+        
+        self.data_dir = os.path.join(self.root_dir, "data")
+        self.config_dir = os.path.join(self.root_dir, "data", "config_sheets")
+        self.results_dir = os.path.join(self.data_dir, "results")
+        self.src_dir = os.path.join(self.root_dir, "src")
+        
+        self._configuration_file = os.path.join(self.root_dir, "configuration.toml")
+        self.configuration = self._read_configuration()
+    
+    def _read_configuration(self) -> dict:
+        configuration = toml.load(self._configuration_file)
+        
+        como_version = configuration["general"]["COMO_version"]
+        configuration["general"]["COMO_version"] = self._get_como_version(como_version)
+        
+        return configuration
+    
+    def update_configuration(
+        self,
+        write_directory: str | Path,
+        config_filename: str = "configuration.toml"
+    ) -> None:
+        with open(os.path.join(write_directory, config_filename), "w") as f:
+            toml.dump(self.configuration, f)
+    
+    def _get_como_version(self, current_version: str):
+        if current_version == "":
+            try:
+                git_tag_command = "git describe --tags --abbrev=0"
+                current_version = subprocess.check_output(git_tag_command, shell=True).decode("utf-8").strip()
+            
+            except subprocess.CalledProcessError as e:
+                raise ValueError("Could not find latest tag") from e
+        
+        return current_version
 
 
 current_dir = os.getcwd()
@@ -28,3 +62,7 @@ work_dir = os.path.join(*directory_list[0:split_index])
 # Add leading "/", as it will not exist right now
 work_dir = os.path.join("/", work_dir)
 configs: Configs = Configs(work_dir)
+
+if __name__ == '__main__':
+    config = Configs("/Users/joshl/PycharmProjects/COMO/main")
+    print(config.configuration)
