2a6561c58f520798fb7ecf23ed08bc5453c1e1db
Josh Loecker <joshloecker@icloud.com>
2023-12-11 12:11:56 -0600

    Fix formatting

--- main/py/tests/test_rnaseq_preprocess.py
+++ main/py/tests/test_rnaseq_preprocess.py
@@ -14,7 +14,8 @@ import rnaseq_preprocess
     [
         # Test using data in COMO_input data
         ["-n", "naiveB immNK", "--gene-format", "Ensembl", "--taxon-id", "9606", "--create-matrix"],
-        ["-n", "dimNK brightNK", "--gene-format", "SYMBOL", "--taxon-id", "human", "--provide-matrix", "--matrix", "COMO_input/counts_matrix.tsv"],
+        ["-n", "dimNK brightNK", "--gene-format", "SYMBOL", "--taxon-id", "human", "--provide-matrix", "--matrix",
+         "COMO_input/counts_matrix.tsv"],
     ]
 )
 def test_arg_input(args: list[str]):
@@ -25,13 +26,13 @@ def test_arg_input(args: list[str]):
     gene_format = args[3]
     taxon_id = args[5]
     matrix_mode = args[6]
-
+    
     parsed = rnaseq_preprocess.parse_args(args)
-
+    
     assert [context_name in parsed.context_names for context_name in context_names.split()]
     assert parsed.gene_format == gene_format
     assert parsed.taxon_id == taxon_id
-
+    
     if matrix_mode == "--create-matrix":
         assert parsed.make_matrix is True
     elif matrix_mode == "--provide-matrix":
