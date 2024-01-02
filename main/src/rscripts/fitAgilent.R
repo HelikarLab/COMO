e46de54b68c379286d44558aa63e2d09700d3e29
Josh Loecker <joshloecker@icloud.com>
2024-01-02 16:50:54 -0600

    Linting update

--- main/src/rscripts/fitAgilent.R
+++ main/src/rscripts/fitAgilent.R
@@ -1,45 +1,47 @@
 suppressPackageStartupMessages(library("stringr"))
+suppressPackageStartupMessages(library("limma"))
 
 # Check if rlogs directory exists, From: https://stackoverflow.com/a/46008094
 # Then prevent messy messages from repeatedly writing to juypter
 work_dir <- getwd()
 r_log_directory <- str_interp("${work_dir}/logs")
 if (!dir.exists(r_log_directory)) { dir.create(r_log_directory) }
-zz <- file(file.path(r_log_directory, "fitAligent.Rout"), open="wt")
-sink(zz, type="message")
+zz <- file(file.path(r_log_directory, "fitAligent.Rout"), open = "wt")
+sink(zz, type = "message")
 
-readagilent <- function(addr,targets){
+readagilent <- function(addr, targets) {
     crd <- getwd()
     setwd(addr)
     # targets <- dir(".", "txt.gz")
-    x <- read.maimages(targets, "agilent", green.only = TRUE)
+    x <- limma::read.maimages(targets, "agilent", green.only = TRUE)
     setwd(crd)
-    y <- backgroundCorrect(x,method="normexp") 
-    y <- normalizeBetweenArrays(y,method="quantile") 
-    yo <- c(y$genes,as.data.frame(y$E))
+    y <- backgroundCorrect(x, method = "normexp")
+    y <- normalizeBetweenArrays(y, method = "quantile")
+    yo <- c(y$genes, as.data.frame(y$E))
     ydf <- data.frame(yo)
-    write.csv(ydf, paste(c(addr, "/ydf_temp.csv"),collapse=""))
+    write.csv(ydf, paste(c(addr, "/ydf_temp.csv"), collapse = ""))
     
     return(ydf)
 }
-fitagilent <- function(addr,target){
+
+fitagilent <- function(addr, target) {
     username <- Sys.info()["user"]
     work_dir <- str_interp("/home/${username}/work")
     crd <- getwd()
     setwd(addr)
     targets <- readTargets(target)
-    x <- read.maimages(dir(), path = addr, source="agilent",green.only=TRUE)
-    y <- backgroundCorrect(x, method="normexp", offset=16)
-    y <- normalizeBetweenArrays(y, method="quantile")
-    y.ave <- avereps(y, ID=y$genes$ProbeName)
+    x <- limma::read.maimages(dir(), path = addr, source = "agilent", green.only = TRUE)
+    y <- backgroundCorrect(x, method = "normexp", offset = 16)
+    y <- normalizeBetweenArrays(y, method = "quantile")
+    y.ave <- avereps(y, ID = y$genes$ProbeName)
     f <- factor(targets$Condition, levels = unique(targets$Condition))
     design <- model.matrix(~0 + f)
     colnames(design) <- levels(f)
     fit <- lmFit(y.ave, design)
-    contrast.matrix <- makeContrasts("control-patient", levels=design)
-    fit2 = contrasts.fit(fit,contrast.matrix)
+    contrast.matrix <- makeContrasts("control-patient", levels = design)
+    fit2 = contrasts.fit(fit, contrast.matrix)
     fit2 = eBayes(fit2)
-    data = topTable(fit2,number = "inf")
+    data = topTable(fit2, number = "inf")
     
     return(data)
-    }
+}
