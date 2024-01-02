94d9cd434ee113d82a9f3ccdfd74b8c18b65fa32
Josh Loecker <joshloecker@icloud.com>
2024-01-02 16:52:40 -0600

    Fixes to incorporate microarray_gen into notebook
    - Fix R script paths
    - Remove duplicate R script paths from classes
    - Drop additional column names in order to perform quantile calculations

--- main/src/instruments.py
+++ main/src/instruments.py
@@ -1,18 +1,22 @@
-#!/usr/bin/python3
-
 import os
 import pandas as pd
 import rpy2.robjects as ro
 import time
 from bioservices import BioDBNet
+from pathlib import Path
+
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.packages import importr
 from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
-import rpy2_api
-from pathlib import Path
 
-affy_r_file_path = Path("./rscripts/fitAffy.R")
-agilent_r_file = Path("./rscript/fitAgilent.R")
+import rpy2_api
+from project import configs
+
+affy_r_filepath = Path(os.path.join(configs.pydir, "rscripts", "fitAffy.R"))
+agilent_r_filepath = Path(os.path.join(configs.pydir, "rscripts", "fitAgilent.R"))
+
+affy_r = open(affy_r_filepath, "r").read()
+agilent_r = open(agilent_r_filepath, "r").read()
 
 
 class RObject:
@@ -24,21 +28,18 @@ class RObject:
         """
         self._home_dir = os.path.expanduser("~")
         pandas2ri.activate()
-
+        
         # Import requried R libraries
         self._affy_library = importr("affy")
         self._limma_library = importr("limma")
-
-        self._affy_R_file = open(f"{self._home_dir}/work/py/rscripts/fitAffy.R", "r").read()
-        self._agilent_R_file = open(f"{self._home_dir}/work/py/rscripts/fitAgilent.R", "r").read()
-
+    
     @property
     def affyio(self):
-        return SignatureTranslatedAnonymousPackage(self._affy_R_file, "affyio")
-
+        return SignatureTranslatedAnonymousPackage(affy_r, "affyio")
+    
     @property
     def agilent(self):
-        return SignatureTranslatedAnonymousPackage(self._agilent_R_file, "agilentio")
+        return SignatureTranslatedAnonymousPackage(agilent_r, "agilentio")
 
 
 class AffyIO:
@@ -52,16 +53,14 @@ class AffyIO:
         # read and translate R functions for handling affy
         # enable R to py conversions
         pandas2ri.activate()
-
+        
         # import R libraries
         self.affy_library = importr("affy")
         self.limma_library = importr("limma")
-
-        self.affy_R_file = open(f"{home_dir}/work/py/rscripts/fitAffy.R", "r").read()
-
+    
     @property
     def affyio(self):
-        affyio_object = SignatureTranslatedAnonymousPackage(self.affy_R_file, "affyio")
+        affyio_object = SignatureTranslatedAnonymousPackage(affy_r, "affyio")
         return affyio_object
 
 
@@ -84,20 +83,20 @@ def agilent_raw(datadir, gsms):
             txts.append(file[0:-3])
             keys.append(file.split("_")[0])
     cols = dict(zip(txts, keys))
-
+    
     targets = pd.DataFrame(gzs, columns=["FileName"], index=txts)
-
+    
     df_agilent = rpy2_api.Rpy2(
-        agilent_r_file,
+        agilent_r_filepath,
         datadir,
         targets
     ).call_function("readagilent")
-
+    
     # df_agilent = RObject().agilent.readagilent(datadir, targets)
     df_agilent = ro.conversion.rpy2py(df_agilent)
     df_temp = pd.read_csv(os.path.join(datadir, "ydf_temp.csv"), header=0)
     df_agilent["ProbeName"] = df_temp["ProbeName"].to_list()
-
+    
     return df_agilent.rename(columns=cols)
 
 
@@ -105,10 +104,12 @@ def agilent_raw(datadir, gsms):
 def readagilent(datadir, gsms, scalefactor=1.1, quantile=0.95, ):
     df_raw = agilent_raw(datadir, gsms)
     df = df_raw.drop(columns=["Row", "Col"])
-    df_negctl = df[df["ControlType"] == -1]
-    df_cutoff = scalefactor * df_negctl.drop(columns=["ControlType"]).quantile(
-        quantile, axis=0
-    )
+    df_negctl: pd.DataFrame = df[df["ControlType"] == -1]
+    
+    # Remove columns from df_negctl because they are not numeric or used in the quantile calculation
+    df_cutoff = scalefactor * df_negctl.drop(columns=[
+        "ControlType", "ProbeName", "SystematicName"
+    ]).quantile(quantile, axis=0)
     df_bool = df.loc[df["ControlType"] == 0, df_cutoff.index.tolist()].gt(
         df_cutoff, axis=1
     )
@@ -125,7 +126,7 @@ def readagilent(datadir, gsms, scalefactor=1.1, quantile=0.95, ):
         df_results.loc[:, col] = 1.0 - quantile
         col = "{}.cel.gz".format(gsm.lower())
         df_results.rename(columns={gsm: col}, inplace=True)
-
+    
     return df_results.drop(["ControlType", "SystematicName"], axis=1)
 
 
@@ -139,17 +140,17 @@ def fetch_entrez_gene_id(
     # Set default values for list of strings
     if output_db is None:
         output_db: list[str] = ["Gene ID", "Ensembl Gene ID"]
-
+    
     s = BioDBNet()
-
+    
     df_maps = pd.DataFrame([], columns=output_db)
     df_maps.index.name = input_db
     i = 0
-
+    
     while i < len(input_values):
         print("retrieve {}:{}".format(i, min(i + 500, len(input_values))))
         df_test = s.db2db(
-            input_db, output_db, input_values[i : min(i + 500, len(input_values))], 9606
+            input_db, output_db, input_values[i: min(i + 500, len(input_values))], 9606
         )
         if isinstance(df_test, pd.DataFrame):
             df_maps = pd.concat([df_maps, df_test], sort=False)
@@ -158,7 +159,7 @@ def fetch_entrez_gene_id(
             time.sleep(delay)
             continue
         i += 500
-        
+    
     return df_maps
 
 
@@ -166,18 +167,18 @@ if __name__ == '__main__':
     home_dir = os.path.expanduser("~")
     # enable R to py conversions
     pandas2ri.activate()
-
+    
     # import R libraries
     affy = importr("affy")
     limma = importr("limma")
-
+    
     # Process the affyio functions
     # This is done using a class because the GSEpipeline also utilizes the R-affyio object
     # This is the best method of keeping this information segregated in a "main" statement,
     # while allowing access to other functions
     # affyio = rpy2_api.Rpy2(r_file_path=affy_r_file_path)
     affyio = AffyIO().affyio
-
+    
     # read and translate R functions for handling agilent
     fit_aligent_R = open(f"{home_dir}/work/py/rscripts/fitAgilent.R", "r").read()
     agilentio = SignatureTranslatedAnonymousPackage(fit_aligent_R, "agilentio")
