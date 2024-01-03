4957140ddb7e626be4378554e6c8d4f8ebd3c512
Josh Loecker <joshloecker@icloud.com>
2024-01-03 12:06:11 -0600

    Add documentation, remove debugging statements

--- main/src/project.py
+++ main/src/project.py
@@ -2,11 +2,13 @@ import os
 from pathlib import Path
 
 
-# find project root dir
 class Configs:
     project_dir: str = None
     
     def __init__(self) -> None:
+        """
+        This class will hold all of the project configurations.
+        """
         if Configs.project_dir is None:
             Configs.project_dir = self._find_project_dir()
         self.rootdir: str = str(Configs.project_dir)
@@ -33,27 +35,3 @@ class Configs:
             "Could not find project directory. "
             "Please ensure you are in the project directory, such as 'COMO' or 'main'."
         )
-
-
-# _current_dir = os.getcwd()
-# _directory_list: list[str] = _current_dir.split("/")
-#
-# # Find the "main" directory
-# _split_index = 0
-# for directory in _directory_list:
-#     _split_index += 1  # Increment the index
-#     if directory == "main":
-#         _split_index += 1
-#         break  # Exit the loop when we find the "main" directory
-#
-# # Unpack items in directory_list
-# # From: https://stackoverflow.com/questions/14826888
-# _work_dir: str = os.path.join(*_directory_list[0:_split_index])
-# print(f"Providing `project.py` with work_dir: {_work_dir}")
-# # Add leading "/", as it will not exist right now
-# _work_dir = os.path.join("/", _work_dir)
-# # configs: _Configs = _Configs(_work_dir)
-
-if __name__ == '__main__':
-    configs = Configs()
-    print(configs.__dict__)
