12be95928aff1024783a2ded64bee6dd0e5864fc
Josh Loecker <joshloecker@gmail.com>
2024-04-26 15:58:50 -0500

    Consolidate usage of command line arguments (#179)
    * Create a new `arguments.py` file to hold standardized arguments across COMO
    
    * Use the newly-created `arguments.py` standard
    
    * Add arguments from `create_context_speciifc_model.py`
    
    * Refactor code to incorporate standardized arguments across COMO
    
    * Include `disease_analysis.py` arguments
    
    * Refactor code to incorporate standardized arguments across COMO
    
    * Add arguments from `knock_out_simulation.py`
    
    * Refactor code to incorporate standardized arguments across COMO
    
    * Added `merge_xomics.py` arguments
    
    * Use standardized `arguments.py`
    
    * Added proteomics arguments
    
    * Use standardized `arguments.py`
    
    * Added `rnaseq_gen.py` arguments
    
    * Use standardized `arguments.py` file
    
    * Added `rnaseq_preproces.py` arguments
    
    * Use new `arguments.py` standard
    
    * Fixed imports based on unit test results
    
    * Fix fast bioservices usage
    
    * Use multi_bioservices for this branch for now becuase we have not merged the related pull request yet
    
    * Fix usage of multi/fast bioservices
    
    * Fix multi_bioservices usage

--- main/src/rnaseq_gen.py
+++ main/src/rnaseq_gen.py
@@ -15,6 +15,19 @@ from project import Configs
 # enable r to py conversion
 pandas2ri.activate()
 
+from arguments import (
+    batch_ratio_arg,
+    config_file_arg,
+    filtering_technique_arg,
+    high_batch_ratio_arg,
+    high_replicate_ratio_arg,
+    library_prep_arg,
+    min_count_arg,
+    min_zfpkm_arg,
+    quantile_arg,
+    replicate_ratio_arg,
+)
+
 configs = Configs()
 r_file_path = Path(configs.root_dir, "src", "rscripts", "rnaseq.R")
 
@@ -23,7 +36,7 @@ def load_rnaseq_tests(filename, context_name, lib_type):
     """
     Load rnaseq results returning a dictionary of test (context, context, cell, etc ) names and rnaseq expression data
     """
-    
+
     def load_dummy_dict():
         savepath = os.path.join(
             configs.root_dir,
@@ -34,12 +47,12 @@ def load_rnaseq_tests(filename, context_name, lib_type):
         )
         dat = pd.read_csv(savepath, index_col="ENTREZ_GENE_ID")
         return "dummy", dat
-    
+
     if (
         not filename or filename == "None"
     ):  # not using this data type, use empty dummy data matrix
         return load_dummy_dict()
-    
+
     inquiry_full_path = os.path.join(
         configs.root_dir, "data", "config_sheets", filename
     )
@@ -48,7 +61,7 @@ def load_rnaseq_tests(filename, context_name, lib_type):
     ):  # check that config file exist (isn't needed to load, but helps user)
         print(f"Error: Config file not found at {inquiry_full_path}")
         sys.exit()
-    
+
     if lib_type == "total":  # if using total RNA-seq library prep
         filename = f"rnaseq_total_{context_name}.csv"
     elif lib_type == "mrna":  # if using mRNA-seq library prep
@@ -60,16 +73,16 @@ def load_rnaseq_tests(filename, context_name, lib_type):
             f"Unsupported RNA-seq library type: {lib_type}. Must be one of 'total', 'mrna', 'sc'."
         )
         sys.exit()
-    
+
     fullsavepath = os.path.join(
         configs.root_dir, "data", "results", context_name, lib_type, filename
     )
-    
+
     if os.path.isfile(fullsavepath):
         data = pd.read_csv(fullsavepath, index_col="ENTREZ_GENE_ID")
         print(f"Read from {fullsavepath}")
         return context_name, data
-    
+
     else:
         print(
             f"{lib_type} gene expression file for {context_name} was not found at {fullsavepath}. This may be "
@@ -94,39 +107,39 @@ def handle_context_batch(
     """
     Handle iteration through each context type and create rnaseq expression file by calling rnaseq.R
     """
-    
+
     rnaseq_config_filepath = os.path.join(
         configs.root_dir, "data", "config_sheets", config_filename
     )
     xl = pd.ExcelFile(rnaseq_config_filepath)
     sheet_names = xl.sheet_names
-    
+
     print(f"Reading config file: {rnaseq_config_filepath}")
-    
+
     for context_name in sheet_names:
         print(f"\nStarting '{context_name}'")
         rnaseq_output_file = f"rnaseq_{prep}_{context_name}.csv"
         rnaseq_output_filepath = os.path.join(
             configs.data_dir, "results", context_name, prep, rnaseq_output_file
         )
-        
+
         rnaseq_input_file = f"gene_counts_matrix_{prep}_{context_name}.csv"
         rnaseq_input_filepath = os.path.join(
             configs.data_dir, "data_matrices", context_name, rnaseq_input_file
         )
-        
+
         if not os.path.exists(rnaseq_input_filepath):
             print(
                 f"Gene counts matrix not found at {rnaseq_input_filepath}, skipping..."
             )
             continue
-        
+
         gene_info_filepath = os.path.join(configs.data_dir, "gene_info.csv")
         os.makedirs(os.path.dirname(rnaseq_output_filepath), exist_ok=True)
-        
+
         print(f"Gene info:\t\t{gene_info_filepath}")
         print(f"Count matrix:\t\t{rnaseq_input_filepath}")
-        
+
         rpy2_api.Rpy2(
             r_file_path=r_file_path,
             counts_matrix_file=rnaseq_input_filepath,
@@ -144,7 +157,7 @@ def handle_context_batch(
             min_count=min_count,
             min_zfpkm=min_zfpkm,
         ).call_function("save_rnaseq_tests")
-        
+
         print(f"Results saved at:\t{rnaseq_output_filepath}")
 
 
@@ -165,120 +178,33 @@ def main(argv):
     least input over gene essentially determination and use the most standardized method of active gene determination.
     flat cutoff of CPM (counts per million) normalized values, check for consensus the same as other methods.
     """
-    
+
     parser = argparse.ArgumentParser(
         prog="rnaseq_gen.py",
         description="Generate a list of active and high-confidence genes from a counts matrix using a user defined "
-                    "at normalization-technique at /work/data/results/<context name>/rnaseq_<context_name>.csv: "
-                    "https://github.com/HelikarLab/FastqToGeneCounts",
+        "at normalization-technique at /work/data/results/<context name>/rnaseq_<context_name>.csv: "
+        "https://github.com/HelikarLab/FastqToGeneCounts",
         epilog="For additional help, please post questions/issues in the MADRID GitHub repo at "
-               "https://github.com/HelikarLab/MADRID or email babessell@gmail.com",
+        "https://github.com/HelikarLab/MADRID or email babessell@gmail.com",
     )
-    parser.add_argument(
-        "-c",
-        "--config-file",
-        type=str,
-        required=True,
-        dest="config_filename",
-        help="Name of config .xlsx file in the /main/data/config_files/. Can be generated using "
-             "rnaseq_preprocess.py or manually created and imported into the Juypterlab",
-    )
-    parser.add_argument(
-        "-r",
-        "--replicate-ratio",
-        type=float,
-        required=False,
-        default=0.5,
-        dest="replicate_ratio",
-        help="Ratio of replicates required for a gene to be active within that study/batch group "
-             "Example: 0.7 means that for a gene to be active, at least 70% of replicates in a group "
-             "must pass the cutoff after normalization",
-    )
-    parser.add_argument(
-        "-g",
-        "--batch-ratio",
-        type=float,
-        required=False,
-        default=0.5,
-        dest="batch_ratio",
-        help="Ratio of groups (studies or batches) required for a gene to be active "
-             "Example: 0.7 means that for a gene to be active, at least 70% of groups in a study must  "
-             "have passed the replicate ratio test",
-    )
-    parser.add_argument(
-        "-rh",
-        "--high-replicate-ratio",
-        type=float,
-        required=False,
-        default=1.0,
-        dest="replicate_ratio_high",
-        help="Ratio of replicates required for a gene to be considered high-confidence. "
-             "High-confidence genes ignore consensus with other data-sources like proteomics or "
-             "microarray. Example: 0.9 means that for a gene to be high-confidence, at least 90% of "
-             "replicates in a group must pass the cutoff after normalization",
-    )
-    parser.add_argument(
-        "-gh",
-        "--high-batch-ratio",
-        type=float,
-        required=False,
-        default=1.0,
-        dest="batch_ratio_high",
-        help="Ratio of groups (studies/batches) required for a gene to be considered high-confidence "
-             "within that group. HHigh-confidence genes ignore consensus with other data-sources like "
-             "proteomics or microarray. Example: 0.9 means that for a gene to be high-confidence, at "
-             "least 90% of groups in a study must have passed the replicate ratio test",
-    )
-    parser.add_argument(
-        "-t",
-        "--filt-technique",
-        type=str,
-        required=False,
-        default="quantile-tpm",
-        dest="technique",
-        help="Technique to normalize and filter counts with. Either 'zfpkm', 'quantile-tpm' or "
-             "'flat-cpm'. More info about each method is discussed in pipeline.ipynb.",
-    )
-    parser.add_argument(
-        "-q",
-        "--quantile",
-        type=int,
-        required=False,
-        default=25,
-        dest="quantile",
-        help="Cutoff used for quantile-tpm normalization and filtration technique. Example: 25 means "
-             "that genes with TPM > 75% percentile wik=ll be considered active for that replicate.",
-    )
-    parser.add_argument(
-        "-m",
-        "--min-count",
-        required=False,
-        default="default",
-        dest="min_count",
-        help="Cutoff used for cpm. Minimum number of counts to be considered expressed, alternatively "
-             "use 'default' to use method outlined in CITATION NEEDED",
-    )
-    parser.add_argument(
-        "-z",
-        "--min-zfpkm",
-        required=False,
-        default="default",
-        dest="min_zfpkm",
-        help="Cutoff used for zfpkm. Minimum zfpkm to be considered expressed, according to the paper, "
-             "CITATION NEEDED, should be between -3 and -2.",
-    )
-    parser.add_argument(
-        "-p",
-        "--library-prep",
-        required=False,
-        default="",
-        dest="prep",
-        help="Library preparation used, will separate samples into groups to only compare similarly "
-             "prepared libraries. For example, mRNA, total-rna, scRNA, etc",
-    )
-    
+
+    filtering_technique_arg["required"] = False
+    filtering_technique_arg["default"] = "quantile-tpm"
+    filtering_technique_arg["choices"] = ["quantile-tpm", "zfpkm", "flat-cpm"]
+    quantile_arg["default"] = 0.25
+
+    parser.add_argument(**batch_ratio_arg)
+    parser.add_argument(**config_file_arg)
+    parser.add_argument(**filtering_technique_arg)
+    parser.add_argument(**high_batch_ratio_arg)
+    parser.add_argument(**high_replicate_ratio_arg)
+    parser.add_argument(**library_prep_arg)
+    parser.add_argument(**min_count_arg)
+    parser.add_argument(**min_zfpkm_arg)
+    parser.add_argument(**quantile_arg)
+    parser.add_argument(**replicate_ratio_arg)
     args = parser.parse_args(argv)
-    
+
     config_filename = args.config_filename
     replicate_ratio = args.replicate_ratio
     batch_ratio = args.batch_ratio
@@ -289,7 +215,7 @@ def main(argv):
     min_count = args.min_count
     prep = args.prep
     min_zfpkm = args.min_zfpkm
-    
+
     if re.search("tpm", technique.lower()) or re.search("quantile", technique.lower()):
         technique = "quantile"
     elif re.search("cpm", technique.lower()):
@@ -301,12 +227,12 @@ def main(argv):
             "Normalization-filtration technique not recognized. Must be 'tpm-quantile', 'cpm', or 'zfpkm'."
         )
         sys.exit()
-    
+
     if int(quantile) > 100 or int(quantile) < 1:
         print("Quantile must be between 1 - 100")
-    
+
     prep = prep.replace(" ", "")
-    
+
     handle_context_batch(
         config_filename,
         replicate_ratio,
