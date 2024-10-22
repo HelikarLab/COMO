a7a68214e76bc1fb6d590863466b5015243d6c58
Josh Loecker <joshloecker@icloud.com>
2024-10-22 15:07:22 -0500

    Fix dependencies; cobamp requires extremely out of date software - trying to remove this dependency

--- main/como/__init__.py
+++ main/como/__init__.py
@@ -4,6 +4,7 @@ from como.como_utilities import stringlist_to_list
 from como.project import Config
 
 __all__ = ["stringlist_to_list", "Config", "RNASeqPreparationMethod"]
+__version__ = "1.10.0"
 
 
 class RNASeqPreparationMethod(Enum):
