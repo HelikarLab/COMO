a17b6e13a602e5a2db3cb9d96072ceb1dc500f22
Josh Loecker <joshloecker@icloud.com>
2024-10-22 14:25:31 -0500

    Only import required items

--- main/como/__init__.py
+++ main/como/__init__.py
@@ -1,1 +1,12 @@
-from .como_utilities import stringlist_to_list
+from enum import Enum
+
+from como.como_utilities import stringlist_to_list
+from como.project import Config
+
+__all__ = ["stringlist_to_list", "Config", "RNASeqPreparationMethod"]
+
+
+class RNASeqPreparationMethod(Enum):
+    TOTAL = "total"
+    MRNA = "mrna"
+    SCRNA = "scrna"
