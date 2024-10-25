5dad45a6b41ed4f038fc77d5960b7cf84e0fefab
Josh Loecker <joshloecker@icloud.com>
2024-10-25 14:22:14 -0500

    refactor: build an empty dataframe as-needed to reduce dependency on existing files

--- main/como/__init__.py
+++ main/como/__init__.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+import pandas as pd
+
 from como.como_utilities import stringlist_to_list
 from como.project import Config
 
@@ -11,3 +13,22 @@ class RNASeqPreparationMethod(Enum):
     TOTAL = "total"
     MRNA = "mrna"
     SCRNA = "scrna"
+
+    @staticmethod
+    def from_string(value: str) -> "RNASeqPreparationMethod":
+        match_value = "".join(c for c in value if c.isascii()).lower()
+
+        match match_value:
+            case "total" | "trna":
+                return RNASeqPreparationMethod.TOTAL
+            case "mrna":
+                return RNASeqPreparationMethod.MRNA
+            case "scrna":
+                return RNASeqPreparationMethod.SCRNA
+            case _:
+                possible_values = [t.value for t in RNASeqPreparationMethod]
+                raise ValueError(f"Filtering technique must be one of {possible_values}; got: {value}")
+
+
+def return_placeholder_data() -> pd.DataFrame:
+    return pd.DataFrame(data=0, index=pd.Index(data=[0], name="entrez_gene_id"), columns=["expressed", "top"])
