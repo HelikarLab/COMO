901e3b32dfd7374dce8224433d6bab134be0f34f
JoshLoecker <JoshLoecker@users.noreply.github.com>
2024-10-29 19:03:39 +0000

    style: format code and imports with `ruff`

--- main/como/proteomics/FileInformation.py
+++ main/como/proteomics/FileInformation.py
@@ -3,6 +3,7 @@ This will hold all relevant information about a single file to download
 
 This should be implemented as a list of objects
 """
+
 import os
 import sys
 from pathlib import Path
@@ -27,7 +28,7 @@ class FileInformation:
     # This allows us to search through ALL instances of every FileInformation with functions declared here
     # From: https://stackoverflow.com/a/17253634
     instances: list = []
-    
+
     def __init__(
         self,
         cell_type: str,
@@ -43,7 +44,7 @@ class FileInformation:
         self.cell_type: str = cell_type
         self.download_url: str = download_url
         self.file_size: int = file_size
-        
+
         # Must check for "None", as we are unable to do study[0] on a None object
         if isinstance(study, str):
             self.study: str = study
@@ -53,29 +54,28 @@ class FileInformation:
             self.study: str = ""
         self.replicate: str = ""
         self.batch: str = f"{study}"
-        
+
         # Base file save paths
         if raw_path is None:
             self.raw_base_path: Path = Path(project.configs.data_dir, "results", cell_type, "proteomics", "raw")
         else:
             self.raw_base_path: Path = raw_path.parent
-        
+
         if mzml_path is None:
             self.mzml_base_path: Path = Path(project.configs.data_dir, "results", cell_type, "proteomics", "mzml")
         else:
             self.mzml_base_path: Path = mzml_path.parent
-        
+
         if sqt_path is None:
             self.sqt_base_path: Path = Path(project.configs.data_dir, "results", cell_type, "proteomics", "sqt")
         else:
             self.sqt_base_path: Path = sqt_path.parent
-        
+
         if intensity_csv is None:
-            self.intensity_csv: Path = Path(project.configs.data_dir, "data_matrices", cell_type,
-                                            f"protein_abundance_matrix_{cell_type}.csv")
+            self.intensity_csv: Path = Path(project.configs.data_dir, "data_matrices", cell_type, f"protein_abundance_matrix_{cell_type}.csv")
         else:
             self.intensity_csv: Path = intensity_csv
-        
+
         # The following variables have inital values set based only on an S# batch, not a replicate
         # The set_replicate function must be called to set the values for a specific replicate, in which these variables will be reset
         # File names
@@ -83,19 +83,19 @@ class FileInformation:
         self.raw_file_name: str = f"{self.base_name}.raw"
         self.mzml_file_name: str = f"{self.base_name}.mzml"
         self.sqt_file_name: str = f"{self.base_name}.target.sqt"
-        
+
         # Full file paths
         self.raw_file_path: Path = Path(self.raw_base_path, self.raw_file_name)
         self.mzml_file_path: Path = Path(self.mzml_base_path, self.mzml_file_name)
         self.sqt_file_path: Path = Path(self.sqt_base_path, self.sqt_file_name)
-        
+
         # Intensity dataframe
         self.base_columns: list[str] = ["uniprot"]
         self.df_columns: list[str] = self.base_columns + [self.batch]
         self.intensity_df: pd.DataFrame = pd.DataFrame(columns=self.df_columns)
-        
+
         FileInformation.instances.append(self)
-    
+
     def set_replicate(self, replicate: str | int):
         """
         This function sets self.replicate, and also resets values that use the "replicate" value before it is used
@@ -105,7 +105,7 @@ class FileInformation:
             self.replicate: str = replicate
         else:
             self.replicate: str = f"R{replicate}"
-        
+
         # "Reset" additional values
         self.batch: str = f"{self.study}{self.replicate}"
         # File names
@@ -113,17 +113,17 @@ class FileInformation:
         self.raw_file_name: str = f"{self.base_name}.raw"
         self.mzml_file_name: str = f"{self.base_name}.mzml"
         self.sqt_file_name: str = f"{self.base_name}.target.sqt"
-        
+
         # Full file paths
         self.raw_file_path: Path = Path(self.raw_base_path, self.raw_file_name)
         self.mzml_file_path: Path = Path(self.mzml_base_path, self.mzml_file_name)
         self.sqt_file_path: Path = Path(self.sqt_base_path, self.sqt_file_name)
-        
+
         # Intensity dataframe
         self.base_columns: list[str] = ["uniprot"]
         self.df_columns: list[str] = self.base_columns + [self.batch]
         self.intensity_df: pd.DataFrame = pd.DataFrame(columns=self.df_columns)
-    
+
     @classmethod
     def filter_instances(cls, cell_type: str):
         """
@@ -131,7 +131,7 @@ class FileInformation:
         """
         sorted_instances: list = sorted(cls.instances, key=lambda x: x.study)
         return [instance for instance in sorted_instances if instance.cell_type == cell_type]
-    
+
     @staticmethod
     def intensity_file_path(cell_type: str) -> Path:
         """
