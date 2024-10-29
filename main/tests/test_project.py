8521b23ebe67cd36e40b952a025a7f5fa212abd8
Josh Loecker <joshloecker@icloud.com>
2024-10-29 11:12:43 -0500

    fix: remove unused imports

--- main/tests/test_project.py
+++ main/tests/test_project.py
@@ -1,8 +1,5 @@
-import os
 from pathlib import Path
 
-# Add parent directory to path, allows us to import the "project.py" file from the parent directory
-# From: https://stackoverflow.com/a/30536516/13885200
 from como import project
 
 
