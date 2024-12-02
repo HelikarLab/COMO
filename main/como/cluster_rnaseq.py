eb0e65daa43fa45ba88690f09f80210ae388a854
Josh Loecker <joshloecker@icloud.com>
2024-12-02 16:23:55 -0600

    refactor: create more structured file
    Signed-off-by: Josh Loecker <joshloecker@icloud.com>

--- main/como/cluster_rnaseq.py
+++ main/como/cluster_rnaseq.py
@@ -1,18 +1,80 @@
+from __future__ import annotations
+
 import argparse
-from pathlib import Path
+from curses.ascii import isdigit
+from dataclasses import dataclass
+from typing import Annotated, Any
 
 import numpy as np
-import rpy2_api
-from project import Config
+from pydantic import BaseModel
 
 from como.utils import stringlist_to_list
 
 
-def main() -> None:
-    """
-    Cluster RNA-seq Data
-    """
+@dataclass
+class _Arguments:
+    context_names: list[str]
+    filtering_technique: str
+    cluster_algorithm: str
+    label_plot: str
+    min_distance: float
+    replicate_ratio: Any
+    batch_ratio: Any
+    num_replicate_neighbors: Any
+    num_batch_neighbors: Any
+    num_context_neighbors: Any
+    min_active_count: int | str
+    quantile: Any
+    seed: int
 
+    def __post_init__(self):
+        self.filtering_technique = self.filtering_technique.lower()
+        self.cluster_algorithm = self.cluster_algorithm.lower()
+
+        if self.seed == -1:
+            self.seed = np.random.randint(0, 100_000)
+
+        if (isdigit(self.min_active_count) and int(self.min_active_count) < 0) or self.min_active_count != "default":
+            raise ValueError("--min-count must be either 'default' or an integer > 0")
+
+        if (isdigit(self.quantile) and 0 > int(self.quantile) > 100) or self.quantile != "default":
+            raise ValueError("--quantile must be either 'default' or an integer between 0 and 100")
+
+        if (isdigit(self.replicate_ratio) and 0 > self.replicate_ratio > 1.0) or self.replicate_ratio != "default":
+            raise ValueError("--rep-ratio must be either 'default' or a float between 0 and 1")
+
+        if (isdigit(self.batch_ratio) and 0 > self.batch_ratio > 1.0) or self.batch_ratio != "default":
+            raise ValueError("--batch-ratio must be either 'default' or a float between 0 and 1")
+
+        if self.filtering_technique.lower() not in {"quantile", "tpm", "cpm", "zfpkm"}:
+            raise ValueError("--technique must be either 'quantile', 'tpm', 'cpm', 'zfpkm'")
+
+        if self.filtering_technique.lower() == "tpm":
+            self.filtering_technique = "quantile"
+
+        if self.cluster_algorithm.lower() not in {"mca", "umap"}:
+            raise ValueError("--clust_algo must be either 'mca', 'umap'")
+
+        if 0 > self.min_distance > 1.0:
+            raise ValueError("--min_dist must be a float between 0 and 1")
+
+        if (
+            isdigit(self.num_replicate_neighbors) and self.num_replicate_neighbors < 1
+        ) or self.num_replicate_neighbors != "default":
+            raise ValueError("--n-neighbors-rep must be either 'default' or an integer > 1")
+
+        if (
+            isdigit(self.num_batch_neighbors) and self.num_batch_neighbors < 1
+        ) or self.num_batch_neighbors != "default":
+            raise ValueError("--n-neighbors-batch must be either 'default' or an integer > 1")
+
+        if (
+            isdigit(self.num_context_neighbors) and self.num_context_neighbors < 1
+        ) or self.num_context_neighbors != "default":
+            raise ValueError("--n-neighbors-context must be either 'default' or an integer > 1")
+
+
+def _parse_args() -> _Arguments:
     parser = argparse.ArgumentParser(
         prog="cluster_rnaseq.py",
         description="Cluster RNA-seq Data using Multiple Correspondence Analysis or UMAP. Clusters at the replicate, "
@@ -21,49 +83,42 @@ def main() -> None:
         "https://github.com/HelikarLab/MADRID or email babessell@gmail.com",
     )
     parser.add_argument(
-        "-n",
         "--context-names",
         type=str,
         required=True,
         dest="context_names",
-        help="""Tissue/cell name of models to generate. If making multiple models in a batch, then
-                             use the format: \"['context1', 'context2', ... etc]\". Note the outer double-quotes and the 
-                             inner single-quotes are required to be interpreted. This a string, not a python list""",
+        help="Tissue/cell name of models to generate",
     )
     parser.add_argument(
-        "-t",
-        "--filt-technique",
+        "--filtering-technique",
         type=str,
         required=True,
-        dest="technique",
+        dest="filtering_technique",
         help="'zfpkm', 'quantile', or 'cpm'",
     )
     parser.add_argument(
-        "-a",
         "--cluster-algorithm",
         type=str,
         required=False,
         default="umap",
-        dest="clust_algo",
-        help="""Clustering algorithm to use. 'mca' or 'umap'.""",
+        dest="cluster_algorithm",
+        help="Clustering algorithm to use. 'mca' or 'umap'.",
     )
     parser.add_argument(
-        "-l",
-        "--label",
+        "--label-plot",
         type=str,
         required=False,
         default=True,
-        dest="label",
-        help="""True to label replicate/batch/context names on the plots. May be ugly for large sets""",
+        dest="label_plot",
+        help="Set to True to label replicate/batch/context names on the plots. May be ugly for large sets",
     )
     parser.add_argument(
-        "-d",
-        "--min-dist",
+        "--min-distance",
         type=float,
         required=False,
         default=0.01,
-        dest="min_dist",
-        help="""Minimum distance for UMAP clustering. Must be between 0 and 1""",
+        dest="min_distance",
+        help="Minimum distance for UMAP clustering. Must be between 0 and 1",
     )
     parser.add_argument(
         "-r",
@@ -71,8 +126,8 @@ def main() -> None:
         type=str,
         required=False,
         default=0.9,
-        dest="rep_ratio",
-        help="""Ratio of genes active in replicates for a batch/study to be active""",
+        dest="replicate_ratio",
+        help="Ratio of genes active in replicates for a batch/study to be active",
     )
     parser.add_argument(
         "-b",
@@ -81,187 +136,69 @@ def main() -> None:
         required=False,
         default=0.9,
         dest="batch_ratio",
-        help="""Ratio of genes active in a batch/study to be active in the context""",
+        help="Ratio of genes active in a batch/study to be active in the context",
     )
     parser.add_argument(
-        "-nr",
-        "--n-neighbors-rep",
+        "--num-replicate-neighbors",
         type=str or float,
         required=False,
         default="default",
-        dest="n_neigh_rep",
-        help="""N nearest neighbors for replicate clustering, 'default' is total number of replicates""",
+        dest="num_replicate_neighbors",
+        help="N nearest neighbors for replicate clustering, 'default' is total number of replicates",
     )
     parser.add_argument(
         "-nb",
-        "--n-neighbors-batch",
+        "--num-batch-neighbors",
         type=str or float,
         required=False,
         default="default",
-        dest="n_neigh_batch",
-        help="""N nearest neighbors for batch clustering, 'default' is total number of batches""",
+        dest="num_batch_neighbors",
+        help="N nearest neighbors for batch clustering, 'default' is total number of batches",
     )
     parser.add_argument(
-        "-nc",
-        "--n-neighbors-context",
+        "--num-context-neighbors",
         type=str or float,
         required=False,
         default="default",
-        dest="n_neigh_cont",
-        help="""N nearest neighbors for context clustering, 'default' is total number of contexts""",
+        dest="num_context_neighbors",
+        help="N nearest neighbors for context clustering, 'default' is total number of contexts",
     )
     parser.add_argument(
-        "-c",
-        "--min-count",
+        "--min-active-count",
         type=str or int,
         required=False,
         default="default",
-        dest="min_count",
-        help="""Ratio of active genes in a batch/study to be active in the context""",
+        dest="min_active_count",
+        help="Ratio of active genes in a batch/study to be active in the context",
     )
     parser.add_argument(
-        "-q",
         "--quantile",
         type=str or int,
         required=False,
         default=0.5,
         dest="quantile",
-        help="""Ratio of active genes in a batch/study to be active in the context""",
+        help="Ratio of active genes in a batch/study to be active in the context",
     )
     parser.add_argument(
-        "-s",
         "--seed",
         type=int,
         required=False,
         default=-1,
         dest="seed",
-        help="""Random seed for clustering algorithm initialization""",
+        help="Random seed for clustering algorithm initialization",
     )
     args = parser.parse_args()
+    args.context_names = stringlist_to_list(args.context_names)
+    return _Arguments(**vars(args))
 
-    context_names = stringlist_to_list(args.context_names)
-    technique = args.technique.lower()
-    clust_algo = args.clust_algo.lower()
-    label = args.label
-    rep_ratio = args.rep_ratio
-    batch_ratio = args.batch_ratio
-    min_count = args.min_count
-    quantile = args.quantile
-    min_dist = args.min_dist
-    n_neigh_rep = args.n_neigh_rep
-    n_neigh_batch = args.n_neigh_batch
-    n_neigh_cont = args.n_neigh_cont
 
-    # Set a random seed if none provided
-    if int(args.seed) == -1:
-        seed = np.random.randint(0, 100000)
-    else:
-        seed = args.seed
+def main():
+    """Cluster RNA-seq Data.
 
-    if isinstance(min_count, str) and min_count.lower() == "default":
-        try:
-            min_count = int(min_count)
-        except ValueError:
-            raise ValueError("--min-count must be either 'default' or an integer > 0")
-    if not isinstance(min_count, str) and min_count < 0:
-        raise ValueError("--min-count must be either 'default' or an integer > 0")
-
-    if isinstance(quantile, str) and not quantile.lower() == "default":
-        try:
-            quantile = int(quantile)
-        except ValueError:
-            raise ValueError("--quantile must be either 'default' or an integer between 0 and 100")
-    if not isinstance(quantile, str) and 0 > quantile > 100:
-        raise ValueError("--quantile must be either 'default' or an integer between 0 and 100")
-
-    if isinstance(rep_ratio, str) and not rep_ratio.lower() == "default":
-        try:
-            rep_ratio = float(rep_ratio)
-        except ValueError:
-            raise ValueError("--rep-ratio must be 'default' or a float between 0 and 1")
-    if not isinstance(rep_ratio, str) and 0 > rep_ratio > 1.0:
-        raise ValueError("--rep-ratio must be 'default' or a float between 0 and 1")
-
-    if isinstance(batch_ratio, str) and not batch_ratio.lower() == "default":
-        try:
-            batch_ratio = float(batch_ratio)
-        except ValueError:
-            raise ValueError("--batch-ratio must be 'default' or a float between 0 and 1")
-    if not isinstance(batch_ratio, str) and 0 > batch_ratio > 1.0:
-        raise ValueError("--batch-ratio must be 'default' or a float between 0 and 1")
-
-    if technique.lower() not in ["quantile", "tpm", "cpm", "zfpkm"]:
-        raise ValueError("--technique must be either 'quantile', 'tpm', 'cpm', 'zfpkm'")
-
-    if technique.lower() == "tpm":
-        technique = "quantile"
-
-    if clust_algo.lower() not in ["mca", "umap"]:
-        raise ValueError("--clust_algo must be either 'mca', 'umap'")
-
-    if not isinstance(min_dist, str) and 0 > min_dist > 1.0:
-        raise ValueError("--min_dist must be a float between 0 and 1")
-
-    if isinstance(n_neigh_rep, str) and not n_neigh_rep.lower() == "default":
-        try:
-            n_neigh_rep = int(n_neigh_rep)
-        except ValueError:
-            raise ValueError(
-                "--n_neigh_rep must be either 'default' or an integer greater than 1 and less than or equal to "
-                "the total number of replicates being clustered across all contexts."
-            )
-    if not isinstance(n_neigh_rep, str) and n_neigh_rep < 2:
-        raise ValueError(
-            "--n_neigh_rep must be either 'default' or an integer greater than 1 and less than or equal to "
-            "the total number of replicates being clustered across all contexts."
-        )
-
-    if isinstance(n_neigh_batch, str) and not n_neigh_batch.lower() == "default":
-        try:
-            n_neigh_batch = int(n_neigh_batch)
-        except ValueError:
-            raise ValueError(
-                "--n_neigh_batch must be either 'default' or an integer greater than 1 and less than or equal to "
-                "the total number of batches being clustered across all contexts."
-            )
-    if not isinstance(n_neigh_batch, str) and n_neigh_batch < 2:
-        raise ValueError(
-            "--n_neigh_batch must be either 'default' or an integer greater than 1 and less than or equal to "
-            "the total number of batches being clustered across all contexts."
-        )
-
-    if isinstance(n_neigh_cont, str) and not n_neigh_cont.lower() == "default":
-        try:
-            n_neigh_cont = int(n_neigh_cont)
-        except ValueError:
-            raise ValueError(
-                "--n_neigh_batch must be either 'default' or an integer greater than 1 and less than or equal to "
-                "the total number of batches being clustered across all contexts."
-            )
-    if not isinstance(n_neigh_cont, str) and n_neigh_cont < 2:
-        raise ValueError(
-            "--n_neigh_context must be either 'default' or an integer greater than 1 and less than or equal to "
-            "the total number of contexts being clustered."
-        )
-
-    cluster_samples = rpy2_api.Rpy2(
-        r_file_path=r_file_path,
-        wd=configs.result_dir,
-        context_names=context_names,
-        technique=technique,
-        clust_algo=clust_algo,
-        label=label,
-        min_dist=min_dist,
-        n_neigh_rep=n_neigh_rep,
-        n_neigh_batch=n_neigh_batch,
-        n_neigh_cont=n_neigh_cont,
-        rep_ratio=rep_ratio,
-        batch_ratio=batch_ratio,
-        quantile=quantile,
-        min_count=min_count,
-        seed=seed,
-    )
-    cluster_samples.call_function("cluster_samples_main")
+    This will need to be onverted from rscripts/cluster_samples.R
+    """
+    args: _Arguments = _parse_args()
+    raise NotImplementedError("This function is not yet implemented.")
 
 
 if __name__ == "__main__":
