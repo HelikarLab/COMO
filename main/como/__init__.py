8662f7c609b2269907ce4bee19933afc9839564e
Josh Loecker <joshloecker@icloud.com>
2025-01-12 14:00:20 -0600

    fix: added Algorithm as top level import

--- main/como/__init__.py
+++ main/como/__init__.py
@@ -1,5 +1,5 @@
 from como.utils import stringlist_to_list
-from como.data_types import LogLevel, FilteringTechnique, AdjustmentMethod, Solver
+from como.data_types import LogLevel, FilteringTechnique, AdjustmentMethod, Solver, Algorithm
 
 
 __all__ = ["stringlist_to_list", "LogLevel", "FilteringTechnique", "AdjustmentMethod", "Solver"]
