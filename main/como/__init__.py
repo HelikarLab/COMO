c454e8df97dd93a6f1dbf6b904de516925ab73e1
Josh Loecker <joshloecker@icloud.com>
2025-01-12 14:01:32 -0600

    fix: added CobraCompartments as top-level import

--- main/como/__init__.py
+++ main/como/__init__.py
@@ -1,6 +1,14 @@
 from como.utils import stringlist_to_list
-from como.data_types import LogLevel, FilteringTechnique, AdjustmentMethod, Solver, Algorithm
+from como.data_types import LogLevel, FilteringTechnique, AdjustmentMethod, Solver, Algorithm, CobraCompartments
 
 
-__all__ = ["stringlist_to_list", "LogLevel", "FilteringTechnique", "AdjustmentMethod", "Solver"]
+__all__ = [
+    "AdjustmentMethod",
+    "Algorithm",
+    "CobraCompartments",
+    "FilteringTechnique",
+    "LogLevel",
+    "Solver",
+    "stringlist_to_list",
+]
 __version__ = "1.11.1"
