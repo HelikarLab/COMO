d65a219c4ceed92201861325e06b41e16d06c807
Josh Loecker <joshloecker@icloud.com>
2025-09-05 11:31:34 -0500

    chore: fix typo
    Signed-off-by: Josh Loecker <joshloecker@icloud.com>

--- tests/unit/test_data_types.py
+++ tests/unit/test_data_types.py
@@ -2,7 +2,7 @@ from como.data_types import SourceTypes
 
 
 def test_source_types():
-    """Validate that source types always go in the order of 'trna', 'mrna', 'scrna', 'protemics'."""
+    """Validate that source types always go in the order of 'trna', 'mrna', 'scrna', 'proteomics'."""
     expected_order = ["trna", "mrna", "scrna", "proteomics"]
     for expected, source in zip(expected_order, SourceTypes):
         expected: str
