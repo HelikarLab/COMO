009fbb23c6b7d38badf4f1d1d02d459d6976f4ba
Josh Loecker <joshloecker@icloud.com>
2025-09-05 11:20:31 -0500

    feat: add identifier column check for gene expression data processing
    Signed-off-by: Josh Loecker <joshloecker@icloud.com>

--- main/como/create_context_specific_model.py
+++ main/como/create_context_specific_model.py
@@ -6,6 +6,7 @@ import sys
 from collections.abc import Sequence
 from io import TextIOWrapper
 from pathlib import Path
+from typing import Literal, cast
 
 import cobra
 import numpy as np
@@ -263,7 +264,18 @@ async def _map_expression_to_reaction(
 
     These rules should be defined in the general genome-scale metabolic model
     """
-    gene_activity = split_gene_expression_data(await _read_file(gene_expression_file), recon_algorithm=recon_algorithm)
+    expression_data = await _read_file(gene_expression_file)
+    identifier_column = next((col for col in ("entrez_gene_id", "ensembl_gene_id") if col in expression_data.columns), "")
+
+    if not identifier_column:
+        raise ValueError(
+            f"At least one column of 'entrez_gene_id' or 'ensembl_gene_id' could not be found in the gene expression file '{gene_expression_file}'"
+        )
+    gene_activity = split_gene_expression_data(
+        expression_data,
+        identifier_column=cast(Literal["ensembl_gene_id", "entrez_gene_id"], identifier_column),
+        recon_algorithm=recon_algorithm,
+    )
     reaction_expression = collections.OrderedDict()
 
     # fmt: off
@@ -281,19 +293,19 @@ async def _map_expression_to_reaction(
         rxn: cobra.Reaction
 
         gene_reaction_rule = rxn.gene_reaction_rule
-        if gene_reaction_rule == "":
+        if not gene_reaction_rule:
             continue
 
         gene_ids = set(re.findall(r"\d+", gene_reaction_rule))
         reaction_expression[rxn.id] = default_expression
         for gene_id in gene_ids:
-            activity = f"{gene_activity.at[gene_id, 'active']}" if gene_id in gene_activity.index else f"{default_expression!s}"
+            activity = gene_activity.at[gene_id, "active"] if gene_id in gene_activity.index else f"{default_expression!s}"
             # replace gene_id with activity, using optional whitespace before and after the gene id
             # Do not replace the whitespace (if it exists) before and after the gene ID
             gene_reaction_rule = re.sub(pattern=rf"\b{gene_id}\b", repl=activity, string=gene_reaction_rule)
 
         try:
-            # We are using eval here because ast.literal_eval is unable to process an evaluable such as `max(-4, -4)`
+            # We are using eval here because ast.literal_eval is unable to process an evaluable such as `max(-4, 0, 1)`
             # This isn't ideal, but ultimately the only other option is writing and maintaining a custom parsing engine, which is too much work
             evaluable_gene_rule = _gene_rule_logical(gene_reaction_rule).replace("{", "(").replace("}", ")")
             reaction_expression[rxn.id] = eval(evaluable_gene_rule)  # noqa: S307
