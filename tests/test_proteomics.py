37be94ba55c5432fcb582a9c22b4a301e6d23d01
Josh Loecker <joshloecker@icloud.com>
2025-11-03 11:00:28 -0600

    format: ruff formatting
    Signed-off-by: Josh Loecker <joshloecker@icloud.com>

--- tests/test_proteomics.py
+++ tests/test_proteomics.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 import aioftp
 import pytest
+
 from como.proteomics.Crux import MZMLtoSQT, RAWtoMZML, SQTtoCSV
 from como.proteomics.FileInformation import FileInformation
 from como.proteomics.FTPManager import Download, Reader, aioftp_client
 from como.proteomics.proteomics_preprocess import ParseCSVInput, PopulateInformation
-
 from tests.fixtures.fixture_ftp_server import fixture_ftp_server, ftp_file_names
 
 
